7b4efa631e3ade7cf6b1c1b7e9d5f17cd0720b26
Laurent Chapon <chapon@ill.fr>
2013-01-19 08:39:09 +0000

    Singleton base class

---
+++ include/utils/Singleton.h
@@ -0,0 +1,61 @@
+/*
+ * nsxtool : Neutron Single Crystal analysis Toolkit
+    ------------------------------------------------------------------------------------------
+    Copyright (C)
+    2012- Laurent C. Chapon Institut Laue-Langevin
+	BP 156
+	6, rue Jules Horowitz
+	38042 Grenoble Cedex 9
+	France
+	chapon[at]ill.fr
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef NSXTOOL_SINGLETON_H_
+#define NSXTOOL_SINGLETON_H_
+
+//! Simple templated singleton class using S. Meyers's implementation
+template <typename T> class Singleton
+{
+public:
+	static T* Instance();
+protected:
+	Singleton();
+	Singleton(const Singleton&);
+	Singleton& operator=(const Singleton&);
+	virtual ~Singleton();
+};
+
+
+template <typename T>
+T* Singleton<T>::Instance()
+{
+	static T instance;
+	return &instance;
+}
+template <typename T>
+Singleton<T>::Singleton()
+{
+}
+
+template <typename T>
+Singleton<T>::~Singleton()
+{
+}
+
+
+#endif /* NSXTOOL_SINGLETON_H_ */
