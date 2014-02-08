010b4a87427595c6b7c0169cd0528dcfb44cf967
Laurent Chapon <chapon@ill.fr>
2014-02-08 20:07:00 +0000

    Test if this version of FindCMinPack works on windows

--- cmake/FindCMinpack.cmake
+++ cmake/FindCMinpack.cmake
@@ -1,44 +1,67 @@
-# - Find CMINPACK
-# Find the native CMINPACK includes and library
-# This module defines
-#  CMINPACK_INCLUDE_DIR, where to find jpeglib.h, etc.
-#  CMINPACK_LIBRARIES, the libraries needed to use CMINPACK.
-#  CMINPACK_FOUND, If false, do not try to use CMINPACK.
-# also defined, but not for general use are
-#  CMINPACK_LIBRARY, where to find the CMINPACK library.
+###############################################################################
+# Find CMinpack
+#
+# This sets the following variables:
+# CMINPACK_FOUND - True if CMinpack was found.
+# CMINPACK_INCLUDE_DIRS - Directories containing the CMinpack include files.
+# CMINPACK_LIBRARIES - Libraries needed to use CMinpack.
+# CMINPACK_LIBRARIES_DEBUG - Libraries needed to use CMinpack, debug version.
+# CMINPACK_DEFINITIONS - Compiler flags for CMinpack.
 
-FIND_PATH(CMINPACK_INCLUDE_DIR cminpack.h PATHS /usr/local/include /usr/include PATH_SUFFIXES cminpack cminpack-1 )
+find_package(PkgConfig)
+pkg_check_modules(PC_CMINPACK cminpack)
+set(CMINPACK_DEFINITIONS ${PC_CMINPACK_CFLAGS_OTHER})
 
-SET(CMINPACK_NAMES ${CMINPACK_NAMES} minpack cminpack)
-FIND_LIBRARY(CMINPACK_LIBRARY
-  NAMES ${CMINPACK_NAMES}
-  PATHS /usr /usr/local
-  PATH_SUFFIXES lib lib64
-  )
+find_path(CMINPACK_INCLUDE_DIR cminpack.h
+          HINTS ${NESTK_ROOT_DIRS_HINTS} ${PC_CMINPACK_INCLUDEDIR} ${PC_CMINPACK_INCLUDE_DIRS} "${CMINPACK_ROOT}" "$ENV{CMINPACK_ROOT}"
+          PATHS "$ENV{PROGRAMFILES}/CMINPACK 1.1.3" "$ENV{PROGRAMW6432}/CMINPACK 1.1.3" 
+          PATH_SUFFIXES include/cminpack-1 include/cminpack include)
 
-IF (CMINPACK_LIBRARY AND CMINPACK_INCLUDE_DIR)
-    SET(CMINPACK_LIBRARIES ${CMINPACK_LIBRARY})
-    SET(CMINPACK_FOUND "YES")
-ELSE (CMINPACK_LIBRARY AND CMINPACK_INCLUDE_DIR)
-  SET(CMINPACK_FOUND "NO")
-ENDIF (CMINPACK_LIBRARY AND CMINPACK_INCLUDE_DIR)
+# Prefer static libraries in Windows over shared ones
+if(WIN32)
+  find_library(CMINPACK_LIBRARY 
+               NAMES cminpack_s cminpack
+               HINTS ${NESTK_ROOT_DIRS_HINTS} ${PC_CMINPACK_LIBDIR} ${PC_CMINPACK_LIBRARY_DIRS} "${CMINPACK_ROOT}" "$ENV{CMINPACK_ROOT}"
+               PATHS "$ENV{PROGRAMFILES}/CMINPACK 1.1.3" "$ENV{PROGRAMW6432}/CMINPACK 1.1.3" 
+               PATH_SUFFIXES lib lib64)
 
+  find_library(CMINPACK_LIBRARY_DEBUG 
+               NAMES cminpack_s-gd cminpack-gd cminpack_s cminpack
+               HINTS ${NESTK_ROOT_DIRS_HINTS} ${PC_CMINPACK_LIBDIR} ${PC_CMINPACK_LIBRARY_DIRS} "${CMINPACK_ROOT}" "$ENV{CMINPACK_ROOT}"
+               PATHS "$ENV{PROGRAMFILES}/CMINPACK 1.1.3" "$ENV{PROGRAMW6432}/CMINPACK 1.1.3" 
+               PATH_SUFFIXES lib lib64)
+else(WIN32)
+  find_library(CMINPACK_LIBRARY 
+               NAMES cminpack
+               HINTS ${NESTK_ROOT_DIRS_HINTS} ${PC_CMINPACK_LIBDIR} ${PC_CMINPACK_LIBRARY_DIRS} "${CMINPACK_ROOT}" "$ENV{CMINPACK_ROOT}"
+               PATH_SUFFIXES lib)
 
-IF (CMINPACK_FOUND)
-   IF (NOT CMINPACK_FIND_QUIETLY)
-      MESSAGE(STATUS "Found CMINPACK: ${CMINPACK_LIBRARIES}")
-   ENDIF (NOT CMINPACK_FIND_QUIETLY)
-ELSE (CMINPACK_FOUND)
-   IF (CMINPACK_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find CMINPACK library")
-   ENDIF (CMINPACK_FIND_REQUIRED)
-ENDIF (CMINPACK_FOUND)
+  find_library(CMINPACK_LIBRARY_DEBUG 
+               NAMES cminpack-gd cminpack
+               HINTS ${NESTK_ROOT_DIRS_HINTS} ${PC_CMINPACK_LIBDIR} ${PC_CMINPACK_LIBRARY_DIRS} "${CMINPACK_ROOT}" "$ENV{CMINPACK_ROOT}"
+               PATH_SUFFIXES lib)
+endif(WIN32)
 
-# Deprecated declarations.
-SET (NATIVE_CMINPACK_INCLUDE_PATH ${CMINPACK_INCLUDE_DIR} )
-GET_FILENAME_COMPONENT (NATIVE_CMINPACK_LIB_PATH ${CMINPACK_LIBRARY} PATH)
+if(NOT CMINPACK_LIBRARY_DEBUG)
+  set(CMINPACK_LIBRARY_DEBUG ${CMINPACK_LIBRARY})
+endif(NOT CMINPACK_LIBRARY_DEBUG)
 
-MARK_AS_ADVANCED(
-  CMINPACK_LIBRARY
-  CMINPACK_INCLUDE_DIR
-  )
+set(CMINPACK_INCLUDE_DIRS ${CMINPACK_INCLUDE_DIR})
+set(CMINPACK_LIBRARIES optimized ${CMINPACK_LIBRARY} debug ${CMINPACK_LIBRARY_DEBUG})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CMinpack DEFAULT_MSG
+    CMINPACK_LIBRARY CMINPACK_INCLUDE_DIR)
+
+mark_as_advanced(CMINPACK_LIBRARY CMINPACK_LIBRARY_DEBUG CMINPACK_INCLUDE_DIR)
+
+if(CMINPACK_FOUND)
+  message(STATUS "CMinPack found (include: ${CMINPACK_INCLUDE_DIRS}, libs: ${CMINPACK_LIBRARIES})")
+  if(WIN32)
+    option(CMINPACK_IS_STATIC "Set to OFF if you use shared cminpack library." ON)
+    mark_as_advanced(CMINPACK_IS_STATIC)
+    if(CMINPACK_IS_STATIC)
+      add_definitions(-DCMINPACK_NO_DLL)
+    endif(CMINPACK_IS_STATIC)
+  endif(WIN32)
+endif(CMINPACK_FOUND)
