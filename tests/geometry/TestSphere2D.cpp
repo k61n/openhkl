336d167f32a18fec3471d983ce239c6157cdf4f8
Eric Pellegrini <pellegrini@ill.fr>
2014-02-28 08:23:24 +0000

    Added TestSphere2D unit tets for testing 2D sphere object

--- tests/geometry/TestSphere2D.cpp
+++ tests/geometry/TestSphere2D.cpp
@@ -8,9 +8,7 @@
 using namespace SX::Geometry;
 using Eigen::Vector2d;
 using Eigen::Vector3d;
-using Eigen::Vector4d;
 using Eigen::Matrix2d;
-using Eigen::Matrix3d;
 
 const double tolerance=1e-5;
 const double tolerance_large=1.0;
@@ -19,18 +17,16 @@ BOOST_AUTO_TEST_CASE(Test_Sphere)
 {
 
 	// Test: the construction of a 3D sphere
-	Vector3d center(3,-2,4);
+	Vector2d center(3,-2);
 	double radius(10);
-	Sphere<double,3> s1(center,radius);
-	Vector3d lower(s1.getLower());
-	Vector3d upper(s1.getUpper());
+	Sphere<double,2> s1(center,radius);
+	Vector2d lower(s1.getLower());
+	Vector2d upper(s1.getUpper());
 
 	BOOST_CHECK_CLOSE(lower[0], -7.0,tolerance);
 	BOOST_CHECK_CLOSE(lower[1],-12.0,tolerance);
-	BOOST_CHECK_CLOSE(lower[2], -6.0,tolerance);
 	BOOST_CHECK_CLOSE(upper[0], 13.0,tolerance);
 	BOOST_CHECK_CLOSE(upper[1],  8.0,tolerance);
-	BOOST_CHECK_CLOSE(upper[2], 14.0,tolerance);
 
 	// Test: the isotropic scaling of a Sphere
 	s1.scale(0.5);
@@ -38,26 +34,22 @@ BOOST_AUTO_TEST_CASE(Test_Sphere)
 	upper = s1.getUpper();
 	BOOST_CHECK_CLOSE(lower[0],-2.0,tolerance);
 	BOOST_CHECK_CLOSE(lower[1],-7.0,tolerance);
-	BOOST_CHECK_CLOSE(lower[2],-1.0,tolerance);
 	BOOST_CHECK_CLOSE(upper[0], 8.0,tolerance);
 	BOOST_CHECK_CLOSE(upper[1], 3.0,tolerance);
-	BOOST_CHECK_CLOSE(upper[2], 9.0,tolerance);
 
 	// Test: the translation of a Sphere
-	s1.translate(Vector3d(3,1,-2));
+	s1.translate(Vector2d(3,1));
 	lower = s1.getLower();
 	upper = s1.getUpper();
 	BOOST_CHECK_CLOSE(lower[0], 1.0,tolerance);
 	BOOST_CHECK_CLOSE(lower[1],-6.0,tolerance);
-	BOOST_CHECK_CLOSE(lower[2],-3.0,tolerance);
 	BOOST_CHECK_CLOSE(upper[0],11.0,tolerance);
 	BOOST_CHECK_CLOSE(upper[1], 4.0,tolerance);
-	BOOST_CHECK_CLOSE(upper[2], 7.0,tolerance);
 
 	// Test: a given point falls inside the Sphere
 	int nSteps(500);
-	Vector3d delta=(upper-lower)/nSteps;
-	Vector4d point(0,0,0,1);
+	Vector2d delta=(upper-lower)/nSteps;
+	Vector3d point(0,0,1);
 	double sum(0.0);
 	for(int i=0;i<=nSteps;++i)
 	{
@@ -65,36 +57,32 @@ BOOST_AUTO_TEST_CASE(Test_Sphere)
 		for(int j=0;j<=nSteps;++j)
 		{
 			point.y() = lower[1]+j*delta[1];
-			for(int k=0;k<=nSteps;++k)
-			{
-				point.z() = lower[2]+k*delta[2];
-				if (s1.isInside(point))
-					sum+=1.0;
-			}
+			if (s1.isInside(point))
+				sum+=1.0;
 		}
 	}
 
-	sum *= ((upper[0]-lower[0])*(upper[1]-lower[1])*(upper[2]-lower[2]))/(nSteps*nSteps*nSteps);
-	BOOST_CHECK_CLOSE(sum,523.6,tolerance_large);
+	sum *= ((upper[0]-lower[0])*(upper[1]-lower[1]))/(nSteps*nSteps);
+	BOOST_CHECK_CLOSE(sum,78.5,tolerance_large);
 
 	s1.translate(-s1.getCenter());
-	Sphere<double,3> s2(Vector3d(10,0,0),1.0);
+	Sphere<double,2> s2(Vector2d(10,0),1.0);
 	BOOST_CHECK_EQUAL(s1.collide(s2),false);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),false);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),false);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),true);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),true);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),true);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),true);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),true);
-	s2.translate(Vector3d(-2,0,0));
+	s2.translate(Vector2d(-2,0));
 	BOOST_CHECK_EQUAL(s1.collide(s2),false);
 
 }
