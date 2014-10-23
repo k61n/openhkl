44794a7d095fdf3e2775ac958ebc4db3acf8e9d7
Eric Pellegrini <pellegrini@ill.fr>
2014-10-23 13:37:52 +0000

    Put convenience functions readIntsFromChar and readDoublesfromChar in a specific module

---
+++ utils/include/Parser.h
@@ -0,0 +1,52 @@
+/*
+ * nsxtool : Neutron Single Crystal analysis toolkit
+ ------------------------------------------------------------------------------------------
+ Copyright (C)
+ 2012- Laurent C. Chapon, Eric Pellegrini
+ Institut Laue-Langevin
+ BP 156
+ 6, rue Jules Horowitz
+ 38042 Grenoble Cedex 9
+ France
+ chapon[at]ill.fr
+ pellegrini[at]ill.fr
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef NSXTOOL_PARSER_H_
+#define NSXTOOL_PARSER_H_
+
+#include <vector>
+
+namespace SX
+{
+
+namespace Utils
+{
+
+//! Method to read a vector of int values from two char pointers, using spirit
+//! This is faster than the C atoi() function.
+void readIntsFromChar(const char* begin, const char* end, std::vector<int>& v);
+
+//! Method to read a vector of double values from two char pointers, using spirit
+void readDoublesFromChar(const char* begin, const char* end, std::vector<double>& v);
+
+} // end namespace Utils
+
+} // end namespace SX
+
+#endif /* NSXTOOL_PARSER_H_ */
---
+++ utils/src/Parser.cpp
@@ -0,0 +1,27 @@
+#include <boost/spirit/include/qi.hpp>
+#include "Parser.h"
+
+
+namespace SX
+{
+
+namespace Utils
+{
+
+void readIntsFromChar(const char* begin, const char* end, std::vector<int>& v)
+{
+	namespace qi = boost::spirit::qi;
+	namespace ascii = boost::spirit::ascii;
+	qi::phrase_parse(begin, end, *qi::int_ >> qi::eoi, ascii::space, v);
+}
+
+void readDoublesFromChar(const char* begin, const char* end, std::vector<double>& v)
+{
+	namespace qi = boost::spirit::qi;
+	namespace ascii = boost::spirit::ascii;
+	qi::phrase_parse(begin, end, *qi::double_ >> qi::eoi, ascii::space, v);
+}
+
+} // end namespace Utils
+
+} // end namespace SX
