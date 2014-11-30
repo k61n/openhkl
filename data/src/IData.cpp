666f1f0c1c1f9a96034649a15f833e2e58edb018
Laurent Chapon <chapon@ill.fr>
2014-11-30 16:12:33 +0000

    Modify HDF5 IO to use HDF5 c++ API instead of C. Added call to blosc_init() to fix segfault on reading.

--- data/src/IData.cpp
+++ data/src/IData.cpp
@@ -8,8 +8,10 @@
 #include "Detector.h"
 #include "Gonio.h"
 #include "Sample.h"
-#include <hdf5.h>
+
+#include "H5Cpp.h"
 #include "blosc_filter.h"
+#include "blosc.h"
 
 namespace SX
 {
@@ -200,130 +202,128 @@ std::size_t IData::getFileSize() const
 
 void IData::saveHDF5(const std::string& filename)
 {
+
+	blosc_init();
+	blosc_set_nthreads(4);
+
 	if (!_inMemory)
 		throw std::runtime_error("Can't save "+_filename+" as HDF5, file not in memory");
 
-	int nrows=_diffractometer->getDetector()->getNRows();
-	int ncols=_diffractometer->getDetector()->getNCols();
+	hsize_t dims[3] = {_nFrames, _nrows,_ncols};
+	hsize_t chunk[3] = {1, _nrows,_ncols};
+	hsize_t count[3] = {1, _nrows,_ncols};
 
-	hsize_t dims[3] = {_nFrames, ncols,nrows};
-	hsize_t  chunk[3] = {1, ncols,nrows};
-	hsize_t  count[3] = {1, ncols,nrows};
+	H5::H5File file(filename.c_str(), H5F_ACC_TRUNC);
 
-	std::string fullName=filename+".h5";
-	hid_t file_id = H5Fcreate(fullName.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	H5::DataSpace* space=new H5::DataSpace(3,dims,NULL);
 
-	/// DATASPACE IS CHUNKED ///
-	hid_t space = H5Screate_simple (3, dims, NULL);
-	hid_t dcpl = H5Pcreate (H5P_DATASET_CREATE);
-	hid_t status = H5Pset_chunk(dcpl, 3, chunk);
+	H5::DSetCreatPropList* plist = new  H5::DSetCreatPropList();
+
+	plist->setChunk(3,chunk);
 
 	char *version, *date;
 	int r;
 	unsigned int cd_values[7];
-	/* But you can also taylor Blosc parameters to your needs */
-	/* 0 to 3 (inclusive) param slots are reserved. */
-	cd_values[4] = 9;       /* compression level */
-	cd_values[5] = 1;       /* 0: shuffle not active, 1: shuffle active */
-	cd_values[6] = BLOSC_LZ4; /* the actual compressor to use */
+	cd_values[4] = 9;       // Highest compression level
+	cd_values[5] = 1;       // Bit shuffling active
+	cd_values[6] = BLOSC_BLOSCLZ; // Seem to be the best compromise speed/compression for diffraction data
 
 	r = register_blosc(&version, &date);
-	std::cout << version << "," << date << std::endl;
-	//if (r>0)
-		r = H5Pset_filter(dcpl, FILTER_BLOSC, H5Z_FLAG_OPTIONAL, 7, cd_values);
+	if (r<=0)
+			throw std::runtime_error("Problem registering BLOSC filter in HDF5 library");
 
-	  hid_t HDF5_datatype;
-	  HDF5_datatype = H5T_NATIVE_UINT32;
-	  hid_t  memspace = H5Screate_simple(3, count, NULL);
-	  hid_t dataset_id = H5Dcreate2(file_id, "data", HDF5_datatype, space, H5P_DEFAULT, dcpl, H5P_DEFAULT);
+	plist->setFilter(FILTER_BLOSC, H5Z_FLAG_OPTIONAL, 7, cd_values);
+
+	H5::DataSpace* memspace=new H5::DataSpace(3,count,NULL);
+
+	H5::DataSet* dset=new H5::DataSet(file.createDataSet("counts", H5::PredType::NATIVE_INT, *space, *plist));
 
 	  hsize_t offset[3];
 	  offset[0] = 0;
 	  offset[1] = 0;
 	  offset[2] = 0;
 
-	  std::cout << "Writting data \n";
 	  for(offset[0]=0; offset[0]<_data.size(); offset[0] += count[0])
 		{
-
-		  herr_t errstatus = H5Sselect_hyperslab(space, H5S_SELECT_SET, offset, NULL, count, NULL);
-		  status = H5Dwrite(dataset_id, HDF5_datatype, memspace, space, H5P_DEFAULT,  _data.at(offset[0]).data());
-		  if(status <0)
-			throw std::runtime_error("Problem writting hdf5 file");
+		  space->selectHyperslab(H5S_SELECT_SET,count,offset,NULL,NULL);
+		  dset->write(_data.at(offset[0]).data(),H5::PredType::NATIVE_INT,*memspace,*space);
 		}
 
-	  H5Sclose(memspace);
-	  H5Sclose(space);
-	  H5Dclose(dataset_id);
-	  H5Fclose(file_id);
-	  H5Pclose(dcpl);
+
+	  delete dset;
+	  delete memspace;
+	  delete space;
+	  delete plist;
+	  file.close();
+
+	  //
+	  blosc_destroy();
 }
 
 void IData::readHDF5(const std::string& filename)
 {
-	std::cout << "Reading data" <<std::endl;
-
-
-	int nrows=_diffractometer->getDetector()->getNRows();
-	int ncols=_diffractometer->getDetector()->getNCols();
-
-	hid_t dcpl = H5Pcreate (H5P_DATASET_CREATE);
-	hsize_t  chunk[3] = {1, ncols,nrows};
-	hid_t status = H5Pset_chunk(dcpl, 3, chunk);
+	// Needed
+	blosc_init();
+	blosc_set_nthreads(4);
 
 	char *version, *date;
-	int r;
-	unsigned int cd_values[7];
-	/* But you can also taylor Blosc parameters to your needs */
-	/* 0 to 3 (inclusive) param slots are reserved. */
-	cd_values[4] = 9;       /* compression level */
-	cd_values[5] = 1;       /* 0: shuffle not active, 1: shuffle active */
-	cd_values[6] = BLOSC_LZ4; /* the actual compressor to use */
+	int r= register_blosc(&version, &date);
 
-	r = register_blosc(&version, &date);
-	std::cout << version << "," << date << std::endl;
-	//if (r>0)
-	r = H5Pset_filter(dcpl, FILTER_BLOSC, H5Z_FLAG_OPTIONAL, 7, cd_values);
+	if (r<=0)
+		throw std::runtime_error("Problem registering BLOSC filter in HDF5 library");
 
-	_data.resize(_nFrames);
-	for (int i=0;i<_nFrames;++i)
-	{
-		_data[i].resize(nrows,ncols);
-	}
+	blosc_init();
+	blosc_set_nthreads(8);
 
-	hid_t HDF5_datatype;
-    HDF5_datatype = H5T_NATIVE_UINT32;
+	H5::H5File file(filename.c_str(), H5F_ACC_RDONLY);
 
-	hid_t file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-	std::cout << "file_id" << file_id << std::endl;
-	hid_t dataset_id = H5Dopen1(file_id, "/data");
-	std::cout << "dataset_id" << dataset_id << std::endl;
-	hid_t space_id = H5Dget_space(dataset_id); //filespace id
-	std::cout << "space_id" << space_id << std::endl;
-	const int ndims = H5Sget_simple_extent_ndims(space_id);
+    H5::DataSet* dset=new H5::DataSet(file.openDataSet("/counts"));
+
+	H5::DataSpace* space=new H5::DataSpace(dset->getSpace());
+
+	// Get rank of data
+	const hsize_t ndims=space->getSimpleExtentNdims();
 	hsize_t dims[ndims];
 	hsize_t maxdims[ndims];
-	H5Sget_simple_extent_dims(space_id, dims, maxdims);
+	// Get dimensions
+	space->getSimpleExtentDims(dims,maxdims);
+
+	if (dims[0]!=_nFrames || dims[1]!=_nrows || dims[2]!=_ncols)
+		throw std::range_error("IData: Data dimensions in HDF5 different from previsouly defined");
+
+	_data.resize(_nFrames);
+	for (std::size_t i=0;i<_nFrames;++i)
+	{
+		_data[i].resize(_nrows,_ncols);
+	}
+
+	// Size of one hyperslab
 	hsize_t  count[3];
-	count[0] = 1; // hyperslab size is one image
-	count[1] = dims[1]; // DIMX * DIMY
-	count[2] = dims[2]; // DIMX * DIMY
-//
-	hid_t memspace_id =  H5Screate_simple(3, count, NULL); // corresponds to the hyperslab
-	std::cout << "memspace_id" << memspace_id << std::endl;
+	count[0] = 1;
+	count[1] = dims[1];
+	count[2] = dims[2];
+
+	H5::DataSpace* memspace=new H5::DataSpace(3,count,NULL);
+
 	hsize_t offset[3];
 	offset[0] = 0;
 	offset[1] = 0;
 	offset[2] = 0;
 
-	//
+	// Read data Slab by slab
 	for(offset[0]=0; offset[0] < _nFrames; offset[0] += count[0])
 	{
-	  herr_t errstatus = H5Sselect_hyperslab(space_id, H5S_SELECT_SET, offset, NULL, count, NULL);
-	  hid_t status = H5Dread(dataset_id, HDF5_datatype, memspace_id, space_id , H5P_DEFAULT,  _data.at(offset[0]).data());
+	  space->selectHyperslab(H5S_SELECT_SET,count,offset,NULL,NULL);
+	  dset->read(_data.at(offset[0]).data(),H5::PredType::NATIVE_INT,*memspace,*space);
 	}
-	H5Dclose(dataset_id);
-	H5Fclose(file_id);
+
+	delete memspace;
+	delete space;
+	delete dset;
+	file.close();
+
+	blosc_destroy();
+
 }
 
 } // end namespace Data
