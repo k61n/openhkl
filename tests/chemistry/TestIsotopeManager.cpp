725bd4c37ea7b3e2ded31271f36f364061d164cc
Eric Pellegrini <pellegrini@ill.fr>
2015-01-05 09:34:16 +0000

    Added unit test conditions for TestIsotopeManager

--- tests/chemistry/TestIsotopeManager.cpp
+++ tests/chemistry/TestIsotopeManager.cpp
@@ -1,21 +1,27 @@
 #define BOOST_TEST_MODULE "Test Isotope Manager"
 #define BOOST_TEST_DYN_LINK
 
+#include <iostream>
+
 #include <boost/test/unit_test.hpp>
 
 #include "IsotopeManager.h"
+#include "Units.h"
 
 using namespace SX::Chemistry;
+using namespace SX::Units;
+
+const double tolerance=1e-6;
 
 BOOST_AUTO_TEST_CASE(Test_Isotope_Manager)
 {
+
+	UnitsManager* um=UnitsManager::Instance();
 
 	IsotopeManager* im=IsotopeManager::Instance();
 
 	Isotope* is=im->getIsotope("H[1]");
 
-
-
-
-
+	BOOST_CHECK_EQUAL(is->getFormalCharge(),0);
+	BOOST_CHECK_CLOSE(is->getMolarMass(),1.00782504*um->get("uma"),tolerance);
 }
