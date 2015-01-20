fd51a19052329023b75db289d9c611fea4535240
Eric Pellegrini <pellegrini@ill.fr>
2015-01-20 07:57:09 +0000

    Added a unit test for formula parser Modified the formula parser to suit the database format

---
+++ tests/utils/TestFormulaParser.cpp
@@ -0,0 +1,28 @@
+#define BOOST_TEST_MODULE "Test Formula Parser"
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+#include <string>
+
+#include "FormulaParser.h"
+
+using namespace SX::Utils;
+
+BOOST_AUTO_TEST_CASE(Test_Formula_Parser)
+{
+
+	formula f;
+
+	std::string form="C[14]H[1]4";
+
+	SX::Utils::FormParser<std::string::iterator> fp;
+	qi::phrase_parse(form.begin(),form.end(),fp,qi::blank,f);
+
+	for (auto it : f)
+		std::cout << boost::fusion::at_c<0>(it) << " "<< boost::fusion::at_c<1>(it) << std::endl;
+
+}
--- utils/include/FormulaParser.h
+++ utils/include/FormulaParser.h
@@ -37,14 +37,17 @@
 
 namespace SX
 {
+
+namespace Utils
+{
 	using namespace boost::spirit;
 	//! Element symbol, isotope (0 if natural), and content
-	typedef boost::fusion::vector3<std::string,unsigned int,double> element;
+	typedef boost::fusion::vector2<std::string,unsigned int> element;
 	//! Formula as a vector of elements
 	typedef std::vector<element> formula;
 	//! @Class: ChemFormulaParser. Use to parse a chemical formula including isotopes.
 	//! The chemical formula is given following the usual conventions with isotopes in brackets.
-	//! For example Gd(156)Mn2O5. This does not check the nature of the chemical element
+	//! For example Gd[156](0.56)Mn4. This does not check the nature of the chemical element
 	//! or isotope validity. This is left to the periodic table.
 	//! The parser is used as usual in spirit given two iterators (begin and end)
 	//! : parse(begin,end,parser)
@@ -52,25 +55,26 @@ namespace SX
 	struct FormParser : qi::grammar<Iterator,formula() >
 	{
 		FormParser(): FormParser::base_type(start)
-			{
+		{
 			using boost::spirit::uint_;
 			using boost::spirit::ascii::char_;
-			using boost::spirit::double_;
 			//! Formula is made of multiple elements.
 			start=*(startelement);
 			//! Symbol followed optionally by isotope, and by content.
-			startelement = symbol >> (isot | qi::attr(0))>> (double_ | qi::attr(1.0));
-			//! if isotope is given, should be inside bracket
-			isot= ( boost::spirit::omit[char_("(")]>> uint_ >> boost::spirit::omit[char_(")")]);
+			startelement = symbol >> (uint_ | qi::attr(1));
 			//! Capital letter followed optionally by small character
-			symbol = (char_("A-Z") >> -(char_("a-z")));
-			}
+			symbol = char_("A-Z") >> -(char_("a-z")) >> -isot;
+			//! if isotope is given, should be inside bracket
+			isot= char_("[") >> uint_ >> char_("]");
+		}
 	private:
 		qi::rule<Iterator,formula()> start;
 		qi::rule<Iterator,element()> startelement;
 		qi::rule<Iterator,unsigned int()> isot;
 		qi::rule<Iterator,std::string()> symbol;
 	};
+
+} // Namespace Utils
 
 } // Namespace SX
 
