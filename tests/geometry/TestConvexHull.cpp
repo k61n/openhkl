687d77168d051d6849a65f4cab1c8ec0d97b4af6
Eric Pellegrini <pellegrini@ill.fr>
2015-02-12 16:58:34 +0000

    Completed the unit test for convexhull

--- tests/geometry/TestConvexHull.cpp
+++ tests/geometry/TestConvexHull.cpp
@@ -3,30 +3,86 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <Eigen/Dense>
-
+#include "Error.h"
+#include "Face.h"
 #include "ConvexHull.h"
 
-using namespace SX::Geometry;
+//! Check that the Hull satisfies the Euler conditions
+template <typename T>
+bool CheckEulerCondition(const SX::Geometry::ConvexHull<T>& chull)
+{
+
+	unsigned int nVertices=chull.getNVertices();
+	unsigned int nEdges=chull.getNEdges();
+	unsigned int nFaces=chull.getNFaces();
+
+	if ( (nVertices - nEdges + nFaces) != 2 )
+		return false;
+	else
+		return true;
+
+	if ( nFaces != (2 * nVertices - 4) )
+		return false;
+	else
+		return true;
+
+	if ( (2 * nEdges) != (3 * nFaces) )
+		return false;
+	else
+		return true;
+}
+
+//! Check that the Hull satisfies the convexity condition. This consists in
+//! checking that the signed volume between every face and every point is positive.
+//! This shows that each point is inside every face and therefore the hull is convex.
+template <typename T>
+bool CheckConvexity(const SX::Geometry::ConvexHull<T>& chull)
+{
+	const auto& faces=chull.getFaces();
+	const auto& vertices=chull.getVertices();
+
+	for (auto & f: faces)
+	{
+		for (auto& v : vertices)
+		{
+			if (f->volumeSign(v) < 0)
+				return false;
+		}
+	}
+	return true;
+}
+
 using Eigen::Vector3d;
-const double tolerance=1e-5;
+const double tolerance=1e-9;
 
 BOOST_AUTO_TEST_CASE(Test_ConvexHull)
 {
 	typedef Eigen::Vector3d vector3;
 
-	// Create a convex hull
-	SX::Geometry::ConvexHull<double> chull;
+	typedef SX::Geometry::ConvexHull<double> CHullDouble;
 
-	// Fill it with a tetrahedron
-	chull.makeVertex(vector3( 0, 0, 0));
-	chull.makeVertex(vector3(10, 0, 0));
-	chull.makeVertex(vector3( 0,10, 0));
-	chull.makeVertex(vector3( 0, 0,10));
+	// Create an empty convex hull
+	CHullDouble chull;
 
-	chull.doubleTriangle();
+	// Checks that the hull can not be updated with 0 point to be processed
+	BOOST_CHECK_THROW(chull.updateHull(),SX::Kernel::Error<CHullDouble>);
 
-	chull.constructHull();
+	// Fill it with three vertices to form a tetrahedron
+	chull.addVertex(vector3( 0, 0, 0));
+	// Checks that the hull can not be updated with only 1 point to be processed
+	BOOST_CHECK_THROW(chull.updateHull(),SX::Kernel::Error<CHullDouble>);
+
+	chull.addVertex(vector3(10, 0, 0));
+	// Checks that the hull can not be updated with only 2 point to be processed
+	BOOST_CHECK_THROW(chull.updateHull(),SX::Kernel::Error<CHullDouble>);
+
+	chull.addVertex(vector3( 0,10, 0));
+	// Checks that the hull can not be updated with only 3 point to be processed
+	BOOST_CHECK_THROW(chull.updateHull(),SX::Kernel::Error<CHullDouble>);
+
+	chull.addVertex(vector3( 0, 0,10));
+	// Checks that with 4 vertices the hull can be built
+	BOOST_CHECK_NO_THROW(chull.updateHull());
 
 	const auto& faces=chull.getFaces();
 	const auto& edges=chull.getEdges();
@@ -37,19 +93,34 @@ BOOST_AUTO_TEST_CASE(Test_ConvexHull)
 	BOOST_CHECK_EQUAL(edges.size(),6);
 	BOOST_CHECK_EQUAL(faces.size(),4);
 
+	//! Checks that the hull satisfies the Euler conditions
+	BOOST_CHECK(CheckEulerCondition(chull));
+
+	//! Checks that the hull satisfies the Convexity condition
+	BOOST_CHECK(CheckConvexity(chull));
+
 	// Fill the convex hull with new vertices in order to make a cube
-	chull.makeVertex(vector3(10,10, 0));
-	chull.makeVertex(vector3(10, 0,10));
-	chull.makeVertex(vector3(0, 10,10));
-	chull.makeVertex(vector3(10,10,10));
+	chull.addVertex(vector3(10,10, 0));
+	chull.addVertex(vector3(10, 0,10));
+	chull.addVertex(vector3(0, 10,10));
+	chull.addVertex(vector3(10,10,10));
 
 	// Update the hull
-	chull.constructHull();
+	chull.updateHull();
 
 	// Check that the number of vertices, edges and faces corresponds to a cube
 	BOOST_CHECK_EQUAL(vertices.size(),8);
 	BOOST_CHECK_EQUAL(edges.size(),18);
 	BOOST_CHECK_EQUAL(faces.size(),12);
+
+	//! Checks that the hull satisfies the Euler conditions
+	BOOST_CHECK(CheckEulerCondition(chull));
+
+	//! Checks that the hull satisfies the Convexity condition
+	BOOST_CHECK(CheckConvexity(chull));
+
+	//! Checks that the volume of the cube is 10*10*10=1000
+	BOOST_CHECK_CLOSE(chull.getVolume(),1000,tolerance);
 
 }
 
