9fbdb2ec604eb1fb68c303b2da217b199ea8cfba
Eric Pellegrini <pellegrini@ill.fr>
2015-02-17 15:05:16 +0000

    Progresses in Material class unit test (to be finished)

--- tests/chemistry/TestMaterial.cpp
+++ tests/chemistry/TestMaterial.cpp
@@ -1,102 +1,65 @@
 #define BOOST_TEST_MODULE "Test Material"
 #define BOOST_TEST_DYN_LINK
 
+#include <iostream>
+
 #include <boost/test/unit_test.hpp>
 
-#include <iostream>
-#include <map>
+#include "Error.h"
+#include "Element.h"
+#include "ElementManager.h"
+#include "Material.h"
+#include "MaterialManager.h"
 
-//#include "Units.h"
-//#include "Isotope.h"
-//#include "Element.h"
-//#include "Material.h"
-//#include "Units.h"
-//
-//using namespace SX::Chemistry;
-//
-//const double tolerance=1;
+const double tolerance=1.0e-9;
+
+using namespace SX::Chemistry;
 
 BOOST_AUTO_TEST_CASE(Test_Material)
 {
-//
-//	// Build a methane material dynamically
-//	Material methane("methane",Material::State::Gaz,Material::FillingMode::NumberOfAtoms);
-//	Element carbon("carbon","C");
-//	Element hydrogen("hydrogen","H");
-//	methane.addElement(&carbon,1);
-//	methane.addElement(&hydrogen,4);
-//	methane.setDensity(1.235);
-//
-//	// Check that the registry of elements has been correctly updated
-//	BOOST_CHECK_EQUAL(Element::getNRegisteredElements(),2);
-//
-//	// Check that the registry of materials has been correctly updated
-//	BOOST_CHECK_EQUAL(Material::getNRegisteredMaterials(),1);
-//
-//	std::map<Element*,double> massFractions=methane.getMassFractions();
-//	BOOST_CHECK_CLOSE(massFractions.at(&carbon),0.75,tolerance);
-//	BOOST_CHECK_CLOSE(massFractions.at(&hydrogen),0.25,tolerance);
-//
-//	// Build another methane material dynamically with a different filling method
-//	Material methane1("methane1",Material::State::Gaz,Material::FillingMode::MoleFraction);
-//	methane1.addElement(&carbon,0.2);
-//	methane1.addElement(&hydrogen,0.8);
-//	methane1.setDensity(1.235);
-//	// Check that this corresponds (chemically) to the same material
-//	BOOST_CHECK_EQUAL(methane==methane1,true);
-//
-//	// Build a water material dynamically
-//	Material water("water",Material::State::Liquid,Material::FillingMode::MoleFraction);
-//	Element oxygen("oxygen","O");
-//	water.addElement(&oxygen,1.0/3.0);
-//	water.addElement(&hydrogen,2.0/3.0);
-//	water.setDensity(1.000);
-//
-//	// Check that the registry of elements has been correctly updated
-//	BOOST_CHECK_EQUAL(Element::getNRegisteredElements(),3);
-//
-//	// Check that the registry of materials has been correctly updated
-//	BOOST_CHECK_EQUAL(Material::getNRegisteredMaterials(),3);
-//
-//	massFractions=water.getMassFractions();
-//	BOOST_CHECK_CLOSE(massFractions.at(&oxygen),8.0/9.0,tolerance);
-//	BOOST_CHECK_CLOSE(massFractions.at(&hydrogen),1.0/9.0,tolerance);
-//
-//	// Build a mixture of materials dynamically
-//	Material mixture("mixture",Material::State::Solid,Material::FillingMode::MassFraction);
-//	mixture.addMaterial(&methane,0.5);
-//	mixture.addMaterial(&water,0.5);
-//	mixture.setDensity(3.0);
-//
-//	// Check that the registry of materials has been correctly updated
-//	BOOST_CHECK_EQUAL(Material::getNRegisteredMaterials(),4);
-//
-//	// Build a methane from the materials database
-//	Material* methane2=Material::buildFromDatabase("CH4");
-//	// Check that the registry of materials has been correctly updated
-//	BOOST_CHECK_EQUAL(Material::getNRegisteredMaterials(),5);
-//	// Check that the registry of elements has not been updated
-//	BOOST_CHECK_EQUAL(Element::getNRegisteredElements(),3);
-//	// Check that it corresponds (chemically) to the methane previously defined
-//	BOOST_CHECK_EQUAL(*methane2==methane,true);
-//
-//	Material cf4("CF4",Material::State::Gaz,Material::FillingMode::NumberOfAtoms);
-//	Element fluorine("fluor","F");
-//	cf4.addElement(&carbon,1);
-//	cf4.addElement(&fluorine,4);
-//
-//	Material he3("He3",Material::State::Gaz,Material::FillingMode::PartialPressure);
-//	Element helium3("helium3");
-//	helium3.addIsotope(Isotope::buildFromDatabase("He[3]"),1.0);
-//	he3.addElement(&helium3,10*SX::Units::Bar);
-//	he3.setTemperature(290.0);
-//	std::cout<<he3.getMu()<<std::endl;
-//
-//	Material* gazmix= new Material("HeCF4",Material::State::Gaz,Material::FillingMode::PartialPressure);
-////	gazmix->addMaterial(&cf4,2*SX::Units::Bar);
-//	gazmix->addMaterial(&he3,10*SX::Units::Bar);
-//	gazmix->setTemperature(290.0);
-//
-//	std::cout<<gazmix->getMu()<<std::endl;
-//
+	MaterialManager* mmgr=MaterialManager::Instance();
+	ElementManager* emgr=ElementManager::Instance();
+
+	// Checks that setting the materials database to a wrong path throws
+	BOOST_CHECK_THROW(mmgr->setDatabasePath("/fsdfs/fsdfsd/blablabla.xml"),SX::Kernel::Error<MaterialManager>);
+
+	// Checks that setting the materials database to a correct path does not throw
+	BOOST_CHECK_NO_THROW(mmgr->setDatabasePath("./materials.xml"));
+
+	// Builds the methane molecule from the materials XML database
+	Material* dbMethane=mmgr->findMaterial("db_methane");
+
+	// Check that the registry of materials has been correctly updated
+	BOOST_CHECK_EQUAL(mmgr->getNRegisteredMaterials(),1);
+
+	// Checks that the methane molecule is made of two elements each of them being a composite of isotopes
+	BOOST_CHECK_EQUAL(dbMethane->getNElements(),2);
+	BOOST_CHECK_EQUAL((*dbMethane)["carbon"]->getNIsotopes(),2);
+	BOOST_CHECK_EQUAL((*dbMethane)["hydrogen"]->getNIsotopes(),3);
+	BOOST_CHECK_THROW((*dbMethane)["blabla"]->getNIsotopes(),SX::Kernel::Error<Material>);
+
+	// Checks the mole fractions of this methane molecule
+	contentsMap moleFractions=dbMethane->getMoleFractions();
+	BOOST_CHECK_EQUAL(moleFractions["carbon"],0.2);
+	BOOST_CHECK_EQUAL(moleFractions["hydrogen"],0.8);
+
+	// Checks the mass fractions of this methane molecule
+	double mCarbon = (*dbMethane)["carbon"]->getMolarMass();
+	double mHydrogen = (*dbMethane)["hydrogen"]->getMolarMass();
+	double mTotal = mCarbon + 4.0*mHydrogen;
+	contentsMap massFractions=dbMethane->getMassFractions();
+	BOOST_CHECK_CLOSE(massFractions["carbon"],mCarbon/mTotal,tolerance);
+	BOOST_CHECK_CLOSE(massFractions["hydrogen"],4.0*mHydrogen/mTotal,tolerance);
+
+	// Build a methane material dynamically and checks that its contents is the same than the one built from the database
+	Material* methane= mmgr->buildMaterial("methane",Material::State::Gaz,Material::FillingMode::NumberOfAtoms);
+	methane->addElement(emgr->findElement("carbon"),1);
+	methane->addElement(emgr->findElement("hydrogen"),4);
+	methane->setDensity(1.235);
+	contentsMap massFractions1=methane->getMassFractions();
+	BOOST_CHECK_EQUAL(massFractions["carbon"],massFractions["carbon"]);
+	BOOST_CHECK_EQUAL(massFractions["hydrogen"],massFractions["hydrogen"]);
+
+	// Check that the registry of materials has been correctly updated
+	BOOST_CHECK_EQUAL(mmgr->getNRegisteredMaterials(),2);
 }
