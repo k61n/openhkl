2e4bc6c885f34be6d7560e8049f3846bf67e1882
Eric Pellegrini <pellegrini@ill.fr>
2015-03-25 09:31:00 +0000

    Removed old build directory of c-blosc from previous stand-alone  installations

--- externals/c-blosc/buid/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/pellegrini/Downloads/c-blosc-master
-# Build directory: /users/pellegrini/Downloads/c-blosc-master/buid
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(blosc)
-SUBDIRS(tests)
-SUBDIRS(bench)
--- externals/c-blosc/buid/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/pellegrini/Downloads/c-blosc-master/tests
-# Build directory: /users/pellegrini/Downloads/c-blosc-master/buid/tests
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test_test_api "test_api")
-ADD_TEST(test_test_basics "test_basics")
