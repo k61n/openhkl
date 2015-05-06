d89d1e2e48aec8ce802636184c8890c98ce88e98
Eric Pellegrini <pellegrini@ill.fr>
2015-05-06 11:49:02 +0000

    Implemented the diffractometer store to build diffractometer object from XML files

---
+++ instrument/include/DiffractometerStore.h
@@ -0,0 +1,69 @@
+/*
+ * nsxtool : Neutron Single Crystal analysis toolkit
+ ------------------------------------------------------------------------------------------
+ Copyright (C)
+ 2012- Laurent C. Chapon, Eric Pellegrini
+ Institut Laue-Langevin
+ BP 156
+ 6, rue Jules Horowitz
+ 38042 Grenoble Cedex 9
+ France
+ chapon[at]ill.fr
+ pellegrini[at]ill.fr
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef NSXTOOL_DIFFRACTOMETERSTORE_H_
+#define NSXTOOL_DIFFRACTOMETERSTORE_H_
+
+#include <memory>
+#include <map>
+#include <set>
+#include <string>
+
+#include "Diffractometer.h"
+#include "Singleton.h"
+
+namespace SX
+{
+
+namespace Instrument
+{
+
+typedef std::set<std::string> diffractometersList;
+typedef std::shared_ptr<Diffractometer> sptrDiffractometer;
+typedef std::map<std::string,sptrDiffractometer> diffractomersMap;
+
+class DiffractometerStore : public Kernel::Singleton<DiffractometerStore,Kernel::Constructor,Kernel::Destructor>
+{
+
+public:
+
+	//! Constructor
+	DiffractometerStore();
+
+	sptrDiffractometer buildDiffractomer(const std::string& name);
+
+	diffractometersList getDiffractometersList() const;
+
+};
+
+} // end namespace Instrument
+
+} // end namespace SX
+
+#endif /* NSXTOOL_DIFFRACTOMETERSTORE_H_ */
---
+++ instrument/src/DiffractometerStore.cpp
@@ -0,0 +1,73 @@
+#include <initializer_list>
+#include <iostream>
+
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+#include "Diffractometer.h"
+#include "DiffractometerStore.h"
+#include "Error.h"
+#include "Path.h"
+
+namespace SX
+{
+
+namespace Instrument
+{
+
+namespace fs=boost::filesystem;
+namespace pt=boost::property_tree;
+namespace xml=boost::property_tree::xml_parser;
+
+DiffractometerStore::DiffractometerStore()
+: Kernel::Singleton<DiffractometerStore,Kernel::Constructor,Kernel::Destructor>()
+{
+}
+
+sptrDiffractometer DiffractometerStore::buildDiffractomer(const std::string& name)
+{
+
+	fs::path diffractometersPath(Utils::Path::getDiffractometersPath());
+	diffractometersPath/=name;
+	diffractometersPath+=".xml";
+
+	property_tree::ptree root;
+	try
+	{
+		xml::read_xml(diffractometersPath.string(),root);
+	}
+	catch (const std::runtime_error& error)
+	{
+		throw SX::Kernel::Error<DiffractometerStore>(error.what());
+	}
+
+	const property_tree::ptree& instrumentNode=root.get_child("instrument");
+
+	sptrDiffractometer diffractometer(new Diffractometer(instrumentNode));
+
+	return diffractometer;
+}
+
+diffractometersList DiffractometerStore::getDiffractometersList() const
+{
+
+	diffractometersList diffractometers;
+
+	fs::path diffractometersPath(Utils::Path::getDiffractometersPath());
+
+	for (const auto& p : boost::make_iterator_range(fs::directory_iterator(diffractometersPath),fs::directory_iterator()))
+	{
+		if (!fs::is_regular_file(p) || p.path().extension() != ".xml")
+			continue;
+		diffractometers.insert(p.path().stem().string());
+	}
+
+	return diffractometers;
+}
+
+} // end namespace Instrument
+
+} // end namespace SX
