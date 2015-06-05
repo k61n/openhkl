1501984984b8670cf128fd53cbd60069ab96da9d
Eric Pellegrini <pellegrini@ill.fr>
2015-06-05 07:09:49 +0000

    Relocated Crystal::removeCol and Crystal::removeRow function to Utils Updated accordingly the file where they were called

--- crystal/src/UBMinimizer.cpp
+++ crystal/src/UBMinimizer.cpp
@@ -6,8 +6,9 @@
 #include "Component.h"
 #include "ComponentState.h"
 #include "Detector.h"
-#include "Gonio.h"
+#include "EigenMatrixOp.h"
 #include "Error.h"
+#include "Gonio.h"
 #include "Peak3D.h"
 #include "Sample.h"
 #include "TransAxis.h"
@@ -19,29 +20,6 @@ namespace SX
 
 namespace Crystal
 {
-
-void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove)
-{
-    unsigned int numRows = matrix.rows()-1;
-    unsigned int numCols = matrix.cols();
-
-    if( rowToRemove < numRows )
-        matrix.block(rowToRemove,0,numRows-rowToRemove,numCols) = matrix.block(rowToRemove+1,0,numRows-rowToRemove,numCols);
-
-    matrix.conservativeResize(numRows,numCols);
-}
-
-void removeColumn(Eigen::MatrixXd& matrix, unsigned int colToRemove)
-{
-    unsigned int numRows = matrix.rows();
-    unsigned int numCols = matrix.cols()-1;
-
-    if( colToRemove < numCols )
-        matrix.block(0,colToRemove,numRows,numCols-colToRemove) = matrix.block(0,colToRemove+1,numRows,numCols-colToRemove);
-
-    matrix.conservativeResize(numRows,numCols);
-}
-
 
 UBFunctor::UBFunctor() : Utils::LMFunctor<double>(), _peaks(0), _detector(nullptr), _sample(nullptr),_source(nullptr), _fixedParameters()
 {
@@ -313,8 +291,8 @@ int UBMinimizer::run(unsigned int maxIter)
 		{
 			if (fParams[i])
 			{
-				removeColumn(JtJ,i-removed);
-				removeRow(JtJ,i-removed);
+				Utils::removeColumn(JtJ,i-removed);
+				Utils::removeRow(JtJ,i-removed);
 				removed++;
 			}
 		}
