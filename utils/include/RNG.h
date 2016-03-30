43560002519c7c3ba6156d7d754d46bb9ed5b85e
Eric Pellegrini <pellegrini@ill.fr>
2016-03-30 09:09:32 +0200

    Added the xor128 random number generator

---
+++ utils/include/RNG.h
@@ -0,0 +1,23 @@
+/*
+ * RNG.h
+ *
+ *  Created on: Mar 29, 2016
+ *      Author: pellegrini
+ */
+
+#ifndef UTILS_INCLUDE_RNG_H_
+#define UTILS_INCLUDE_RNG_H_
+
+namespace SX
+{
+
+namespace Utils
+{
+
+double xor128(void);
+
+} /* namespace Utils */
+
+} /* namespace SX */
+
+#endif /* UTILS_INCLUDE_RNG_H_ */
