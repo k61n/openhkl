91bc962373a289c306af42bc90ddb7834c7ea7f7
Eric Pellegrini <pellegrini@ill.fr>
2016-04-25 14:34:04 +0200

    Added Monochromator class to handle monochromatrs bound to a source

---
+++ instrument/include/Monochromator.h
@@ -0,0 +1,104 @@
+/*
+ * Monochromator.h
+ *
+ *  Created on: Apr 8, 2016
+ *      Author: pellegrini
+ */
+
+#ifndef INSTRUMENT_INCLUDE_MONOCHROMATOR_H_
+#define INSTRUMENT_INCLUDE_MONOCHROMATOR_H_
+
+#include <string>
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace SX
+{
+
+namespace Instrument
+{
+
+namespace proptree=boost::property_tree;
+
+class Monochromator
+{
+
+public:
+
+	Monochromator();
+
+	Monochromator(const std::string& name);
+
+	~Monochromator();
+
+	Monochromator(const Monochromator& other);
+
+	Monochromator(const proptree::ptree& node);
+
+	Monochromator& operator=(const Monochromator& other);
+
+	//! Return the name for this monochromator
+	const std::string& getName() const;
+
+	//! Set the name for this monochromator
+	void setName(const std::string& name);
+
+	//! Return the wavelength for this monochromator
+	double getWavelength() const;
+
+	//! Set the wavelength for this monochromator
+	void setWavelength(double wavelength);
+
+	//! Return the full width at half maximum of the wavelength for this monochromator
+	double getFWHM() const;
+
+	//! Set the full width at half maximum of the wavelength for this monochromator
+	void setFWHM(double fwhm);
+
+	//! Return the width for this monochromator
+	double getWidth() const;
+
+	//! Set the width for this monochromator
+	void setWidth(double width);
+
+	//! Return height for this monochromator
+	double getHeight() const;
+
+	//! Set the height for this monochromator
+	void setHeight(double height);
+
+	//! Return the offset of the wavelength of this monochromator
+	double getOffset() const;
+
+	//! Set the offset of the wavelength of this monochromator
+	void setOffset(double offset);
+
+	//! Fix the offset of the wavelength of this monochromator
+	void setOffsetFixed(bool offsetFixed);
+
+	//! Return whether the offset of the wavelength of this monochromator is fixed or not
+	bool isOffsetFixed() const;
+
+private:
+
+	std::string _name;
+
+	double _wavelength;
+
+	double _offset;
+
+	double _offsetFixed;
+
+	double _fwhm;
+
+	double _width;
+
+	double _height;
+
+};
+
+} /* namespace Instrument */
+
+} /* namespace SX */
+
+#endif /* INSTRUMENT_INCLUDE_MONOCHROMATOR_H_ */
---
+++ instrument/src/Monochromator.cpp
@@ -0,0 +1,164 @@
+/*
+ * Monochromator.cpp
+ *
+ *  Created on: Apr 8, 2016
+ *      Author: pellegrini
+ */
+
+#include "Monochromator.h"
+#include "Units.h"
+
+namespace SX
+{
+
+namespace Instrument
+{
+
+Monochromator::Monochromator()
+: _name(""),
+  _wavelength(1.0),
+  _offset(0.0),
+  _offsetFixed(true),
+  _fwhm(0.1),
+  _width(0.01),
+  _height(0.01)
+{
+}
+
+Monochromator::Monochromator(const std::string& name)
+: _name(name),
+  _wavelength(1.0),
+  _offset(0.0),
+  _offsetFixed(true),
+  _fwhm(0.1),
+  _width(0.01),
+  _height(0.01)
+{
+}
+
+Monochromator::Monochromator(const Monochromator& other)
+: _name(other._name),
+  _wavelength(other._wavelength),
+  _offset(other._offset),
+  _offsetFixed(other._offsetFixed),
+  _fwhm(other._fwhm),
+  _width(other._width),
+  _height(other._height)
+{
+}
+
+Monochromator::Monochromator(const proptree::ptree& node)
+: _offset(0.0),
+  _offsetFixed(true)
+{
+	_name=node.get<std::string>("name");
+	_wavelength=node.get<double>("wavelength");
+	_fwhm=node.get<double>("fwhm");
+	Units::UnitsManager* um=SX::Units::UnitsManager::Instance();
+
+	// Set the source slit width from the property tree node
+	const proptree::ptree& widthNode = node.get_child("width");
+	double units=um->get(widthNode.get<std::string>("<xmlattr>.units"));
+	_width=widthNode.get_value<double>();
+	_width *= units;
+
+	// Set the source slit height from the property tree node
+	const proptree::ptree& heightNode = node.get_child("height");
+	units=um->get(heightNode.get<std::string>("<xmlattr>.units"));
+	_height=heightNode.get_value<double>();
+	_height *= units;
+
+}
+
+Monochromator& Monochromator::operator=(const Monochromator& other)
+{
+	if (this!=&other)
+	{
+		_name = other._name;
+		_wavelength = other._wavelength;
+		_offset - other._offset;
+		_offsetFixed = other._offsetFixed;
+		_fwhm = other._fwhm;
+		_width = other._width;
+		_height = other._height;
+	}
+	return *this;
+}
+
+Monochromator::~Monochromator()
+{
+}
+
+const std::string& Monochromator::getName() const
+{
+	return _name;
+}
+
+void Monochromator::setName(const std::string& name)
+{
+	_name = name;
+}
+
+double Monochromator::getWavelength() const
+{
+	return _wavelength + _offset;
+}
+
+void Monochromator::setWavelength(double wavelength)
+{
+	_wavelength = wavelength;
+}
+
+double Monochromator::getFWHM() const
+{
+	return _fwhm;
+}
+
+void Monochromator::setFWHM(double fwhm)
+{
+	_fwhm = fwhm;
+}
+
+double Monochromator::getWidth() const
+{
+	return _width;
+}
+
+void Monochromator::setWidth(double width)
+{
+	_width = width;
+}
+
+double Monochromator::getHeight() const
+{
+	return _height;
+}
+
+void Monochromator::setHeight(double height)
+{
+	_height = height;
+}
+
+double Monochromator::getOffset() const
+{
+	return _offset;
+}
+
+void Monochromator::setOffset(double offset)
+{
+	_offset = offset;
+}
+
+void Monochromator::setOffsetFixed(bool offsetFixed)
+{
+	_offsetFixed = offsetFixed;
+}
+
+bool Monochromator::isOffsetFixed() const
+{
+	return _offsetFixed;
+}
+
+} /* namespace Instrument */
+
+} /* namespace SX */
