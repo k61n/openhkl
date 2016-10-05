4dfbd3dc9ca43adf14658b708b84f86a55c620b0
Eric Pellegrini <pellegrini@ill.fr>
2016-10-05 14:46:42 +0200

    Progresses in windows gitflow implementation

--- build_server/windows/run_tests.bat
+++ build_server/windows/run_tests.bat
@@ -6,5 +6,7 @@ rem set PATH=C:\Users\ci\Libraries\libtiff\lib;%PATH%
 rem set PATH=C:\Program Files\HDF_Group\HDF5\1.8.17\bin;%PATH%
 rem set PATH=%BUILD_DIR%\nsxlib\Release;%PATH%
 
+cd %CI_PROJECT_DIR%\build
+
 ctest -V
 
