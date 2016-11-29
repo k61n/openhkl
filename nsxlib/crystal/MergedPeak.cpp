25d1871d46995be6ad5d530932d4013bd71a1e62
jmfisher <j.fisher@fz-juelich.de>
2016-11-29 15:08:30 +0100

    begin definition of MergedPeak class

---
+++ nsxlib/crystal/MergedPeak.cpp
@@ -0,0 +1,77 @@
+/*
+ * nsxtool : Neutron Single Crystal analysis toolkit
+ ------------------------------------------------------------------------------------------
+ Copyright (C)
+ 2016- Laurent C. Chapon, Eric Pellegrini, Jonathan Fisher
+
+ Institut Laue-Langevin
+ BP 156
+ 6, rue Jules Horowitz
+ 38042 Grenoble Cedex 9
+ France
+ chapon[at]ill.fr
+ pellegrini[at]ill.fr
+
+ Forshungszentrum Juelich GmbH
+ 52425 Juelich
+ Germany
+ j.fisher[at]fz-juelich.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <cmath>
+#include <stdexcept>
+
+#include "MergedPeak.h"
+
+
+namespace SX
+{
+namespace Crystal
+{
+
+MergedPeak::MergedPeak(SpaceGroup &grp):
+    _grp{grp}, _peaks{}, _intensity{0}, _sigma{0}
+{
+
+}
+
+bool MergedPeak::addPeak(sptrPeak3D peak)
+{
+    bool equiv = false;
+
+    // no peaks added to list yet: so add the new peak
+    if (_peaks.size() == 0)
+        equiv = true;
+    // otherwise check if it is equivalent to the existing peaks
+    else
+        equiv = _grp.isEquivalent(_hkl.cast<double>(), peak->getMillerIndices());
+}
+
+Eigen::Vector3i MergedPeak::getIndex() const
+{
+    return _hkl;
+}
+
+
+
+}
+}
+
+
+
+
