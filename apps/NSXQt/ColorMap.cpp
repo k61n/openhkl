0625754bf4aec4a1e69b66e9b4b06c43582d6d3c
jmfisher <j.fisher@fz-juelich.de>
2016-12-05 16:41:28 +0100

    add new Mat2QImage overloads

--- apps/NSXQt/ColorMap.cpp
+++ apps/NSXQt/ColorMap.cpp
@@ -3,20 +3,41 @@
 
 QImage Mat2QImage(int* src, int rows, int cols,int xmin, int xmax, int ymin, int ymax,int colorMax)
 {
-
     if (xmin<0 || xmax> cols || ymin<0 || ymax>rows)
         return QImage();
 
     QImage dest(cols,rows,QImage::Format_RGB32);
 
     src+=ymin*cols;
-    for (int y = ymin; y <= ymax; ++y)
-    {
+    for (int y = ymin; y <= ymax; ++y) {
         QRgb* destrow = (QRgb*)(dest.scanLine(y-ymin));
         int x;
         for (x = xmin; x <= xmax; ++x)
             destrow[x-xmin] = BlueWhite(*(src+x),colorMax);
         src+=cols;
+    }
+    return dest;
+}
+
+QImage Mat2QImage(const Eigen::MatrixXd& m, const QRectF& rect, int colorMax)
+{
+    int xmin = rect.left();
+    int xmax = rect.right();
+    int ymin = rect.top();
+    int ymax = rect.bottom();
+
+    const int rows = m.rows();
+    const int cols = m.cols();
+
+    if (xmin<0 || xmax> cols || ymin<0 || ymax>rows)
+        return QImage();
+
+    QImage dest(cols, rows, QImage::Format_RGB32);
+
+    for (int y = ymin; y <= ymax; ++y) {
+        QRgb* destrow = (QRgb*)(dest.scanLine(y-ymin));
+        for (int x = xmin; x <= xmax; ++x)
+            destrow[x-xmin] = BlueWhited(m(y, x), colorMax);
     }
     return dest;
 }
--- apps/NSXQt/ColorMap.h
+++ apps/NSXQt/ColorMap.h
@@ -1,18 +1,27 @@
 #ifndef COLORMAP_H
 #define COLORMAP_H
 #include <QImage>
+#include <Eigen/Dense>
 
 
-inline QRgb BlueWhite(int v,int vmax)
+inline QRgb BlueWhite(int v, int vmax)
 {
     double mm=1.0/(double)vmax;
     int r=255-v*(255*mm);
     return (v>vmax ? 0xff0000ff : ((0xffu << 24) | r << 16 | r << 8 | 0xff ) );
 }
 
+inline QRgb BlueWhited(double v, double vmax)
+{
+    double mm=1.0/vmax;
+    int r=std::round(255-v*(255*mm));
+    return (v>vmax ? 0xff0000ff : ((0xffu << 24) | r << 16 | r << 8 | 0xff ) );
+}
+
+
 // Get an image from an 2D arrays of (rows,cols) with range (xmin:xmax,ymin:ymax) and
 // intensity coded with colorMax as brightest.
 QImage Mat2QImage(int* src, int rows, int cols,int xmin, int xmax, int ymin, int ymax,int colorMax);
-
+QImage Mat2QImage(const Eigen::MatrixXd& m, const QRectF& rect, int colorMax);
 
 #endif // COLORMAP_H
