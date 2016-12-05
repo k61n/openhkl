6c32323606246eeac4794095727e87dd8572e94a
jmfisher <j.fisher@fz-juelich.de>
2016-12-05 12:32:11 +0100

    begin work on peak fitting dialog

--- apps/NSXQt/dialogs/PeakFitDialog.cpp
+++ apps/NSXQt/dialogs/PeakFitDialog.cpp
@@ -1,8 +1,47 @@
+/*
+ * nsxtool : Neutron Single Crystal analysis toolkit
+ ------------------------------------------------------------------------------------------
+ Copyright (C)
+ 2016- Laurent C. Chapon, Eric Pellegrini, Jonathan Fisher
+
+ Institut Laue-Langevin
+ BP 156
+ 6, rue Jules Horowitz
+ 38042 Grenoble Cedex 9
+ France
+ chapon[at]ill.fr
+ pellegrini[at]ill.fr
+
+ Forshungszentrum Juelich GmbH
+ 52425 Juelich
+ Germany
+ j.fisher[at]fz-juelich.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <QDebug>
+
 #include "dialogs/PeakFitDialog.h"
 #include "ui_PeakFitDialog.h"
+#include "models/SessionModel.h"
 
-PeakFitDialog::PeakFitDialog(QWidget *parent) :
+PeakFitDialog::PeakFitDialog(SessionModel* session, QWidget *parent) :
     QDialog(parent),
+    _session(session),
     ui(new Ui::PeakFitDialog)
 {
     ui->setupUi(this);
@@ -11,4 +50,9 @@ PeakFitDialog::PeakFitDialog(QWidget *parent) :
 PeakFitDialog::~PeakFitDialog()
 {
     delete ui;
+}
+
+void PeakFitDialog::on_pushButton_clicked()
+{
+    qDebug() << "peak fit button clicked!";
 }
--- apps/NSXQt/dialogs/PeakFitDialog.h
+++ apps/NSXQt/dialogs/PeakFitDialog.h
@@ -1,7 +1,45 @@
+/*
+ * nsxtool : Neutron Single Crystal analysis toolkit
+ ------------------------------------------------------------------------------------------
+ Copyright (C)
+ 2016- Laurent C. Chapon, Eric Pellegrini, Jonathan Fisher
+
+ Institut Laue-Langevin
+ BP 156
+ 6, rue Jules Horowitz
+ 38042 Grenoble Cedex 9
+ France
+ chapon[at]ill.fr
+ pellegrini[at]ill.fr
+
+ Forshungszentrum Juelich GmbH
+ 52425 Juelich
+ Germany
+ j.fisher[at]fz-juelich.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #ifndef PEAKFITDIALOG_H
 #define PEAKFITDIALOG_H
 
 #include <QDialog>
+#include <memory>
+
+class SessionModel;
 
 namespace Ui {
 class PeakFitDialog;
@@ -12,11 +50,15 @@ class PeakFitDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit PeakFitDialog(QWidget *parent = 0);
+    explicit PeakFitDialog(SessionModel* session, QWidget *parent = 0);
     ~PeakFitDialog();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::PeakFitDialog *ui;
+    SessionModel* _session;
 };
 
 #endif // PEAKFITDIALOG_H
