79c57956f31fe07fb8c7348b297edd1e83b5816c
jmfisher <j.fisher@fz-juelich.de>
2016-12-13 16:23:35 +0100

    speed up color map calculation

--- apps/NSXQt/ColorMap.cpp
+++ apps/NSXQt/ColorMap.cpp
@@ -61,29 +61,12 @@ static QRgb cmap_poly(const Arr& rs, const Arr& gs, const Arr& bs, double t)
     g *= 255.0;
     b *= 255.0;
 
-    int ir = std::min(std::lround(r), 255l);
-    int ig = std::min(std::lround(g), 255l);
-    int ib = std::min(std::lround(b), 255l);
+    int ir = int(r+0.5);
+    int ig = int(g+0.5);
+    int ib = int(b+0.5);
 
-    return QColor(ir, ig, ib).rgb();
+    return qRgb(ir, ig, ib);
 }
-
-// precomputed global constants used for color maps
-const std::array<double, 10> ViridisCMap::_r = {
-    0.26199569,   0.76259414,  -7.95549936,  28.42312215, -50.08511333,
-      19.34939787,  43.08721907, -11.35989349, -50.97404283,  29.49258413
-};
-
-const std::array<double, 10> ViridisCMap::_g = {
-    0.00418753,  1.4837755,  -0.8924195,  -0.1797478,  1.00381965,  0.59462808,
-     -0.48846077, -1.07553209, -0.59690997,  1.05359406
-};
-
-const std::array<double, 10> ViridisCMap::_b = {
-    0.32239281,   1.94232169,  -6.74189755 , 13.57191937, -14.96017142,
-      1.05841996 , 13.84594501,  -2.33633239 ,-19.99350254,  13.43282862
-};
-
 
 QImage ColorMap::matToImage(const Eigen::MatrixXi& source, const QRect& rect, int colorMax, bool log)
 {
@@ -128,10 +111,32 @@ QRgb BlueWhiteCMap::color(double v, double vmax)
     double mm = 1.0/vmax;
     long r =std::lround(255.0 - v*(255*mm));
 
-    return QColor(r, r, 255).rgb();
+    return qRgb(r, r, 255);
 }
 
 QRgb ViridisCMap::color(double v, double vmax)
 {
     return cmap_poly(_r, _g, _b, v/vmax);
 }
+
+
+// what follows below are precomuted coefficients of polynomials giving greyscale -> RGB color maps
+// taken by fitting cmap profiles from matplotlib. The color maps are named after the corresponding
+// color maps in matplotlib
+
+// viridis color map coefficients
+const std::array<double, 10> ViridisCMap::_r = {
+    0.26199569,   0.76259414,  -7.95549936,  28.42312215, -50.08511333,
+      19.34939787,  43.08721907, -11.35989349, -50.97404283,  29.49258413
+};
+
+const std::array<double, 10> ViridisCMap::_g = {
+    0.00418753,  1.4837755,  -0.8924195,  -0.1797478,  1.00381965,  0.59462808,
+     -0.48846077, -1.07553209, -0.59690997,  1.05359406
+};
+
+const std::array<double, 10> ViridisCMap::_b = {
+    0.32239281,   1.94232169,  -6.74189755 , 13.57191937, -14.96017142,
+      1.05841996 , 13.84594501,  -2.33633239 ,-19.99350254,  13.43282862
+};
+
