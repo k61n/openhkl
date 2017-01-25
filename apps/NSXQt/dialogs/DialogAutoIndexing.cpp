ab2319f5e4fcb9d21a6333af7f922e956e5076de
Eric Pellegrini <pellegrini@ill.fr>
2017-01-25 14:50:44 +0100

    completed DialogAutoIndexing class

--- apps/NSXQt/dialogs/DialogAutoIndexing.cpp
+++ apps/NSXQt/dialogs/DialogAutoIndexing.cpp
@@ -4,18 +4,23 @@
 #include <map>
 #include <string>
 
+#include <QMenu>
+
 #include <Eigen/Dense>
 
+#include <QItemSelectionModel>
 #include <QMessageBox>
 #include <QStandardItemModel>
 #include <QtDebug>
 
+#include "Experiment.h"
 #include "FFTIndexing.h"
 #include "GruberReduction.h"
 #include "IData.h"
 #include "NiggliReduction.h"
 #include "UBMinimizer.h"
 #include "models/CollectedPeaksModel.h"
+#include "models/CollectedPeaksDelegate.h"
 
 using SX::Crystal::tVector;
 using SX::Crystal::BravaisType;
@@ -24,30 +29,26 @@ using SX::Crystal::FFTIndexing;
 using SX::Crystal::NiggliReduction;
 using SX::Crystal::UBMinimizer;
 using SX::Crystal::UBSolution;
+using SX::Data::IData;
 
-DialogAutoIndexing::DialogAutoIndexing(std::shared_ptr<Experiment> experiment, QWidget *parent):
+DialogAutoIndexing::DialogAutoIndexing(std::shared_ptr<Experiment> experiment, std::vector<sptrPeak3D> peaks, QWidget *parent):
     QDialog(parent),
     ui(new Ui::DialogAutoIndexing),
-    _experiment(experiment)
+    _experiment(experiment),
+    _peaks(peaks)
 {
     ui->setupUi(this);
 
-    std::map<std::string,std::shared_ptr<IData>>  datamap=_experiment->getData();
-    std::vector<std::shared_ptr<SX::Data::IData>> datav;
+    setModal(true);
 
-    auto func = [&](std::pair<std::string,std::shared_ptr<SX::Data::IData>> value){datav.push_back(value.second);};
-
-    std::for_each(datamap.begin(), datamap.end(), func);
-
-    CollectedPeaksModel *model = new CollectedPeaksModel();
-    model->setPeaks(datav);
-    model->setUnitCells(_experiment->getDiffractometer()->getSample()->getUnitCells());
-    ui->collectedPeaks->setModel(model);
-
-    _peaks = model->getPeaks();
+    _unitCells = _experiment->getDiffractometer()->getSample()->getUnitCells();
+    for (auto uc : _unitCells)
+        ui->unitCells->addItem(QString::fromStdString(uc->getName()));
 
     connect(ui->index,SIGNAL(clicked()),this,SLOT(autoIndex()));
 
+    // Accept solution and set Unit-Cell
+    connect(ui->solutions->verticalHeader(),SIGNAL(sectionDoubleClicked(int)),this,SLOT(selectSolution(int)));
 }
 
 DialogAutoIndexing::~DialogAutoIndexing()
@@ -57,14 +58,24 @@ DialogAutoIndexing::~DialogAutoIndexing()
 
 void DialogAutoIndexing::autoIndex()
 {
+    int nPeaks = _peaks.size();
     // Check that a minimum number of peaks have been selected for indexing
-    QModelIndexList selectedPeaks = ui->collectedPeaks->selectionModel()->selectedRows();
-    int nPeaks = selectedPeaks.count();
     if (nPeaks < 10)
     {
         QMessageBox::warning(this, tr("NSXTool"),tr("Need at least 10 peaks for autoindexing"));
         return;
     }
+
+    if (_experiment == nullptr)
+        return;
+
+    if (ui->unitCells->count() == 0)
+    {
+        QMessageBox::warning(this, tr("NSXTool"),tr("No unit cells defined for autoindexing"));
+        return;
+    }
+
+    sptrUnitCell selectedUnitCell = _unitCells[ui->unitCells->currentIndex()];
 
     // Clear the current solution list
     _solutions.clear();
@@ -72,10 +83,8 @@ void DialogAutoIndexing::autoIndex()
     // Store Q vectors at rest
     std::vector<Eigen::Vector3d> qvects;
     qvects.reserve(nPeaks);
-    for (const auto& index : selectedPeaks)
+    for (auto peak : _peaks)
     {
-        auto peak=_peaks[index.row()];
-        qDebug()<<index.row()<<index.column();
         if (peak->isSelected() && !peak->isMasked())
             qvects.push_back(peak->getQ());
     }
@@ -87,15 +96,16 @@ void DialogAutoIndexing::autoIndex()
     indexing.addVectors(qvects);
 
     int nSolutions = ui->maxNumSolutions->value();
+    int nStacks = ui->nStacks->value();
     // Find the best tvectors
-    std::vector<tVector> tvects=indexing.findOnSphere(30, nSolutions);
+    std::vector<tVector> tvects=indexing.findOnSphere(nStacks, nSolutions);
 
     auto source = _experiment->getDiffractometer()->getSource();
     auto detector = _experiment->getDiffractometer()->getDetector();
     auto sample = _experiment->getDiffractometer()->getSample();
 
-    std::vector<std::pair<UnitCell,double>> new_solutions;
-    new_solutions.reserve(nSolutions*nSolutions*nSolutions);
+    std::vector<std::pair<UnitCell,double>> newSolutions;
+    newSolutions.reserve(nSolutions*nSolutions*nSolutions);
 
     for (int i = 0; i < nSolutions; ++i)
     {
@@ -110,50 +120,51 @@ void DialogAutoIndexing::autoIndex()
                 if (v1.dot(v2.cross(v3)) > 20.0)
                 {
                     UnitCell cell = UnitCell::fromDirectVectors(v1, v2, v3);
-                    new_solutions.push_back(std::make_pair(cell, 0.0));
+                    newSolutions.push_back(std::make_pair(cell, 0.0));
                 }
             }
         }
     }
 
-    qDebug() << "" << new_solutions.size()<< " possible solutions found";
-    qDebug() << "Refining solutions and diffractometers offsets";
+    qDebug() << "" << newSolutions.size()<< " possible solutions found";
+    qDebug() << "Refining solutions";
 
     //#pragma omp parallel for
-    for (int idx = 0; idx < new_solutions.size(); ++idx) {
+    for (int idx = 0; idx < newSolutions.size(); ++idx) {
 
         UBMinimizer minimizer;
         minimizer.setSample(sample);
         minimizer.setDetector(detector);
         minimizer.setSource(source);
 
-        UnitCell cell = new_solutions[idx].first;
+        UnitCell cell = newSolutions[idx].first;
+        cell.setName(selectedUnitCell->getName());
+        cell.setHKLTolerance(selectedUnitCell->getHKLTolerance());
 
         // Only the UB matrix parameters are used for fit
         int nParameters= 10 + sample->getNAxes() + detector->getNAxes();
         for (int i = 9; i < nParameters; ++i)
             minimizer.refineParameter(i,false);
 
-        double hklTol = ui->hklTolerance->value();
-        int maxNumSolutions = ui->maxNumSolutions->value();
         int success = 0;
-        for (const auto& index : selectedPeaks)
+        for (auto peak : _peaks)
         {
-            auto peak = _peaks[index.row()];
-            if (peak->hasIntegerHKL(cell,hklTol) && peak->isSelected() && !peak->isMasked())
+            Eigen::RowVector3d hkl;
+            bool indexingSuccess = peak->getMillerIndices(cell,hkl,true);
+            if (indexingSuccess && peak->isSelected() && !peak->isMasked())
             {
-                minimizer.addPeak(*peak);
+                minimizer.addPeak(*peak,hkl);
                 ++success;
             }
         }
 
-        if (success < maxNumSolutions)
+        // The number of peaks must be at least for a proper minimization
+        if (success < 10)
             continue;
 
         Eigen::Matrix3d M = cell.getReciprocalStandardM();
         minimizer.setStartingUBMatrix(M);
         int ret = minimizer.runGSL(100);
-
         if (ret == 1)
         {
             UBSolution sln = minimizer.getSolution();
@@ -167,6 +178,10 @@ void DialogAutoIndexing::autoIndex()
             {
                 continue;
             }
+
+            cell.setName(selectedUnitCell->getName());
+            cell.setHKLTolerance(selectedUnitCell->getHKLTolerance());
+
             double tolerance = ui->niggliTolerance->value();
             NiggliReduction niggli(cell.getMetricTensor(), tolerance);
             Eigen::Matrix3d newg, P;
@@ -195,28 +210,29 @@ void DialogAutoIndexing::autoIndex()
 
             double score=0.0;
             double maxscore=0.0;
-            for (const auto& index : selectedPeaks)
+            for (auto peak : _peaks)
             {
-                auto peak = _peaks[index.row()];
                 if (peak->isSelected() && !peak->isMasked())
                 {
                     maxscore++;
-                    if (peak->hasIntegerHKL(cell,hklTol))
+                    Eigen::RowVector3d hkl;
+                    bool indexingSuccess = peak->getMillerIndices(cell,hkl,true);
+                    if (indexingSuccess)
                         score++;
                 }
             }
             // Percentage of indexing
             score /= 0.01*maxscore;
-            new_solutions[idx].first = cell;
-            new_solutions[idx].second = score;
+            newSolutions[idx].first = cell;
+            newSolutions[idx].second = score;
         }
         minimizer.resetParameters();
     }
 
-    _solutions.reserve(new_solutions.size());
+    _solutions.reserve(newSolutions.size());
 
     // remove the false solutions
-    for (auto&& it = new_solutions.begin(); it != new_solutions.end(); ++it)
+    for (auto&& it = newSolutions.begin(); it != newSolutions.end(); ++it)
         if (it->second > 0.1)
             _solutions.push_back(*it);
 
@@ -229,6 +245,7 @@ void DialogAutoIndexing::autoIndex()
 
 void DialogAutoIndexing::buildSolutionsTable()
 {
+
     // Sort solutions by decreasing quality.
     // For equal quality, smallest volume is first
     typedef std::pair<UnitCell,double> Soluce;
@@ -252,6 +269,7 @@ void DialogAutoIndexing::buildSolutionsTable()
     model->setHorizontalHeaderItem(6,new QStandardItem("Volume"));
     model->setHorizontalHeaderItem(7,new QStandardItem("Bravais Type"));
     model->setHorizontalHeaderItem(8,new QStandardItem("Quality"));
+
     // Display solutions
     for (unsigned int i=0;i<_solutions.size();++i)
     {
@@ -283,3 +301,13 @@ void DialogAutoIndexing::buildSolutionsTable()
     ui->solutions->setModel(model);
 }
 
+void DialogAutoIndexing::selectSolution(int index)
+{
+    *_unitCells[ui->unitCells->currentIndex()] = _solutions[index].first;
+
+    QString solutionNumber = QString::number(index+1);
+    QString selectedUnitCellName = ui->unitCells->currentText();
+    QMessageBox::information(this, tr("NSXTool"),tr("Solution %1 set to %2 unit cell").arg(solutionNumber,selectedUnitCellName));
+
+    emit cellUpdated(_unitCells[ui->unitCells->currentIndex()]);
+}
--- apps/NSXQt/dialogs/DialogAutoIndexing.h
+++ apps/NSXQt/dialogs/DialogAutoIndexing.h
@@ -1,13 +1,15 @@
 #ifndef DIALOGAUTOINDEXING_H
 #define DIALOGAUTOINDEXING_H
 
-#include <QDialog>
-
 #include <memory>
 #include <vector>
 
+#include <QDialog>
+#include <QItemSelection>
+
 #include "Peak3D.h"
 #include "UnitCell.h"
+#include "Types.h"
 
 namespace Ui
 {
@@ -28,6 +30,7 @@ class Peak3D;
 
 using SX::Instrument::Experiment;
 using SX::Crystal::UnitCell;
+using SX::Crystal::sptrUnitCell;
 using SX::Crystal::sptrPeak3D;
 
 class DialogAutoIndexing : public QDialog
@@ -35,7 +38,7 @@ class DialogAutoIndexing : public QDialog
     Q_OBJECT
 
 public:
-    explicit DialogAutoIndexing(std::shared_ptr<Experiment> experiment, QWidget *parent = 0);
+    explicit DialogAutoIndexing(std::shared_ptr<Experiment> experiment, const std::vector<sptrPeak3D> peaks, QWidget *parent=0);
     ~DialogAutoIndexing();
 
     void buildSolutionsTable();
@@ -44,6 +47,13 @@ public slots:
 
     void autoIndex();
 
+private slots:
+
+    void selectSolution(int);
+
+signals:
+
+    void cellUpdated(sptrUnitCell);
 
 private:
     Ui::DialogAutoIndexing *ui;
@@ -52,8 +62,9 @@ private:
 
     std::vector<sptrPeak3D> _peaks;
 
-    std::vector<std::pair<UnitCell,double>> _solutions;
+    SX::Crystal::CellList _unitCells;
 
+    std::vector<std::pair<UnitCell,double>> _solutions;
 };
 
 #endif // DIALOGAUTOINDEXING_H
