5afb916a927797b15521fe888a5f8346aa435623
jmfisher <j.fisher@fz-juelich.de>
2017-01-27 11:05:17 +0100

    fix typo; clean/tidy code

--- apps/NSXQt/dialogs/DialogRefineUnitCell.h
+++ apps/NSXQt/dialogs/DialogRefineUnitCell.h
@@ -16,14 +16,17 @@ class DialogRefineUnitCell;
 
 using SX::Crystal::sptrPeak3D;
 using SX::Crystal::sptrUnitCell;
-using  SX::Instrument::sptrExperiment;
+using SX::Instrument::sptrExperiment;
 
 class DialogRefineUnitCell : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit DialogRefineUnitCell(std::shared_ptr<SX::Instrument::Experiment> experiment, sptrUnitCell unitCell, const std::vector<sptrPeak3D>& peaks, QWidget *parent = 0);
+    explicit DialogRefineUnitCell(std::shared_ptr<SX::Instrument::Experiment> experiment,
+                                  sptrUnitCell unitCell,
+                                  std::vector<sptrPeak3D> peaks,
+                                  QWidget *parent = 0);
     ~DialogRefineUnitCell();
     void setLatticeParams();
     void setSampleOffsets();
@@ -33,13 +36,12 @@ public:
     void setSolution(const SX::Crystal::UBSolution& solution);
 
 signals:
-
     void cellUpdated(sptrUnitCell);
 
 private slots:
-    void refineParameter(bool checked,int i);
-    void cellSampleHasChanged(int i,int j);
-    void cellDetectorHasChanged(int i,int j);
+    void refineParameter(bool checked, int i);
+    void cellSampleHasChanged(int i, int j);
+    void cellDetectorHasChanged(int i, int j);
     void refineParameters();
     void createOffsetsTables();
     void resetParameters();
