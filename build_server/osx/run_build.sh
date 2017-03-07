bf0e6becfc8c19cf05c71bad222abc7daf0894a9
Eric Pellegrini <pellegrini@ill.fr>
2017-03-07 18:29:51 +0100

    add gsl path on macos build script

--- build_server/osx/run_build.sh
+++ build_server/osx/run_build.sh
@@ -4,7 +4,7 @@ cd ${CI_PROJECT_DIR}
 
 mkdir ./build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_QTAPPS=ON -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.6.0:/usr/local/Cellar/gsl/2.3 -DCMAKE_INSTALL_PREFIX=. ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_QTAPPS=ON -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.6.0;/usr/local/Cellar/gsl/2.3 -DCMAKE_INSTALL_PREFIX=. ..
 make -j4 -l4
 make install
 
