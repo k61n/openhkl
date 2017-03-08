c43b3c660296217c7b672fff0791908114b0c47d
Eric Pellegrini <pellegrini@ill.fr>
2017-03-08 09:03:23 +0100

    added gsl path to CMAKE_PREFIX_PATH in macos run_deploy script

--- build_server/osx/run_deploy.sh
+++ build_server/osx/run_deploy.sh
@@ -5,7 +5,7 @@ cd ${CI_PROJECT_DIR}
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_QTAPPS=ON -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.6.0/ ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_QTAPPS=ON -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH="/usr/local/Cellar/qt5/5.6.0;/usr/local/Cellar/gsl/2.3" ..
 make -j4 -l4
 make install
 
