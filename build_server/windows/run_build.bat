4bfb24bcbb77cefc485a846b1a3a79cd6d9f2040
Eric Pellegrini <pellegrini@ill.fr>
2017-03-08 11:49:24 +0100

    added GSL_ROOT_DIR env var to windows build script

--- build_server/windows/run_build.bat
+++ build_server/windows/run_build.bat
@@ -1,5 +1,6 @@
 mkdir build
 cd build
+set GSL_ROOT_DIR=C:\Users\ci\Libraries\gsl
 set HDF5_ROOT=C:\Program Files\HDF_Group\HDF5\1.8.17
 set QTDIR=C:\Qt\Qt5.6.1\5.6\msvc2015_64
 set PATH=%QTDIR%\bin;%PATH%
@@ -8,7 +9,6 @@ set BOOST_LIBRARYDIR=c:\local\boost_1_61_0\lib64-msvc-14.0
 set FFTW_DIR=C:\Users\ci\Libraries\fftw
 set PATH=C:\Users\ci\Libraries\eigen;%PATH%
 set PATH=C:\Users\ci\Libraries\libtiff;%PATH%
-set PATH=C:\Users\ci\Libraries\gsl;%PATH%
 set PATH=C:\Program Files\LLVM;%PATH%
 call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat"
 cmake .. -G"Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release
