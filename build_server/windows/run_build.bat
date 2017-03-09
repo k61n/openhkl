014d3274c9ef56eb823772fe6d8a48be20f11bc1
Eric Pellegrini <pellegrini@ill.fr>
2017-03-09 11:31:37 +0100

    updated the windows build script

--- build_server/windows/run_build.bat
+++ build_server/windows/run_build.bat
@@ -1,12 +1,12 @@
 mkdir build
 cd build
-set GSL_ROOT_DIR=C:\Users\ci\Libraries\gsl
 set HDF5_ROOT=C:\Program Files\HDF_Group\HDF5\1.8.17
 set QTDIR=C:\Qt\Qt5.6.1\5.6\msvc2015_64
 set PATH=%QTDIR%\bin;%PATH%
 set BOOST_ROOT=c:\local\boost_1_61_0
 set BOOST_LIBRARYDIR=c:\local\boost_1_61_0\lib64-msvc-14.0
 set FFTW_DIR=C:\Users\ci\Libraries\fftw
+set PATH=C:\temp\pkg-config-0.26\bin;%PATH%
 set PATH=C:\Users\ci\Libraries\eigen;%PATH%
 set PATH=C:\Users\ci\Libraries\libtiff;%PATH%
 set PATH=C:\Program Files\LLVM;%PATH%
