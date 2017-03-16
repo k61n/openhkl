fa4bf643197f1de49fdf7ffc42fb62e20bcaa8af
Eric Pellegrini <pellegrini@ill.fr>
2017-03-16 14:52:03 +0100

    added saveDatabase method

--- nsxlib/chemistry/ChemicalDatabaseManager.h
+++ nsxlib/chemistry/ChemicalDatabaseManager.h
@@ -1,14 +1,6 @@
-/*
- * ChemicalDatabaseManager.h
- *
- *  Created on: Mar 15, 2017
- *      Author: pellegrini
- */
-
 #ifndef NSXLIB_CHEMISTRY_CHEMICALDATABASEMANAGER_H_
 #define NSXLIB_CHEMISTRY_CHEMICALDATABASEMANAGER_H_
 
-#include <iostream>
 #include <map>
 #include <stdexcept>
 #include <string>
@@ -16,8 +8,11 @@
 #include <boost/filesystem.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
+#include <boost/version.hpp>
 
 #include "../kernel/Singleton.h"
+
+#define BOOST_MINOR BOOST_VERSION/100 % 1000
 
 namespace SX {
 
@@ -25,6 +20,7 @@ namespace Chemistry {
 
 using boost::filesystem::exists;
 using boost::property_tree::xml_parser::read_xml;
+using boost::property_tree::xml_parser::write_xml;
 using boost::property_tree::ptree;
 
 using SX::Kernel::Constructor;
@@ -49,6 +45,8 @@ public:
     ChemicalObjectDatabase& getDatabase();
 
     void loadDatabase(const std::string& filename);
+
+    void saveDatabase(std::string filename="") const;
 
 private:
 
@@ -93,26 +91,49 @@ const typename ChemicalDatabaseManager<ChemicalObject>::ChemicalObjectDatabase& 
 template <typename ChemicalObject>
 void ChemicalDatabaseManager<ChemicalObject>::loadDatabase(const std::string& filename)
 {
-    std::cout<<filename<<std::endl;
-
     // The given path does not exists, throws
     if (!exists(filename)) {
         return;
     }
-
-    std::cout<<filename<<std::endl;
 
     _database.clear();
 
     // Parse and load the whole Isotope database in memory
     ptree root;
     read_xml(filename,root);
-    for (const auto& node : root.get_child(ChemicalObject::DatabaseRootNode))
+    for (const auto& node : root.get_child(ChemicalObject::DatabaseParentNode))
     {
+    	if (node.first.compare(ChemicalObject::DatabaseNode)!=0) {
+    		continue;
+    	}
+
         sptrChemicalObject chemObj(new ChemicalObject(node.second));
         _database.insert(std::make_pair(chemObj->getName(),chemObj));
-        std::cout<<node.first<<" --> "<<chemObj->getName()<<" "<<_database.size()<<std::endl;
     }
+}
+
+template <typename ChemicalObject>
+void ChemicalDatabaseManager<ChemicalObject>::saveDatabase(std::string filename) const
+{
+    ptree root;
+	ptree& rootNode = root.add(ChemicalObject::DatabaseParentNode,"");
+
+    for (const auto& chemObject : _database)
+    {
+        auto chemObjectNode = chemObject.second->writeToXML();
+        rootNode.add_child(ChemicalObject::DatabaseNode,chemObjectNode);
+    }
+
+    if (filename.empty())
+        filename=ChemicalObject::DatabasePath;
+
+#if BOOST_MINOR <= 55
+    boost::property_tree::xml_writer_settings<char> settings('\t', 1);
+#else
+    auto settings = boost::property_tree::xml_writer_make_settings<std::string>('\t', 1);
+#endif
+    write_xml(filename,root,std::locale(),settings);
+
 }
 
 } // end namespace Chemistry
