50b582606e0f06649e3d3e7c54790285e48dcd48
Eric Pellegrini <pellegrini@ill.fr>
2017-03-16 14:50:41 +0100

    updated TestIsotope

--- tests/chemistry/TestIsotope.cpp
+++ tests/chemistry/TestIsotope.cpp
@@ -28,4 +28,6 @@ BOOST_AUTO_TEST_CASE(Test_Isotope)
 
     // Checks some of the property of the isotope
     BOOST_CHECK_CLOSE(is->getProperty<double>("molar_mass"),1.00782504*um->get("g_per_mole"),tolerance);
+
+    mgr->saveDatabase("toto.xml");
 }
