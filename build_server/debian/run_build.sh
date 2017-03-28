c890218e4713716d0ad6219790367942b2bf0f55
Jamie Hall <hall@ill.eu>
2017-03-28 13:25:46 +0200

    Update run_build.sh

--- build_server/debian/run_build.sh
+++ build_server/debian/run_build.sh
@@ -5,6 +5,6 @@ cd ${CI_PROJECT_DIR}
 mkdir ./build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_CPACK=OFF -DBUILD_QTAPPS=ON -DCMAKE_INSTALL_PREFIX=./ ..
-make -j2 -l2
+make -j8 -l8
 make install
 
