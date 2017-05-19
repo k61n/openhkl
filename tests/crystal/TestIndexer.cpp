56fd671b75517151c1c98225183226316cdbe550
Eric Pellegrini <pellegrini@ill.fr>
2017-05-19 10:46:32 +0200

    removed unused Indexer class

--- tests/crystal/TestIndexer.cpp
+++
@@ -1,9 +0,0 @@
-#define BOOST_TEST_MODULE "Test Indexer"
-#define BOOST_TEST_DYN_LINK
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(Test_Indexer)
-{
-#pragma message "test not implemented"
-}
