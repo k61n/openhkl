f742e95498301cfae122b3b3d0896b9f38889d1e
Prachi Chavda <prachischavda1@gmail.com>
2017-05-22 16:16:16 +0200

    add new python tests

---
+++ tests/python/TestILLAsciiData.py
@@ -0,0 +1,16 @@
+import pynsx as nsx
+import numpy as np
+import ctypes as c
+import unittest
+
+class TestAABB3D(testILL):
+
+    auto factory = DataReadFactory.Instance();
+    
+   
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+  
