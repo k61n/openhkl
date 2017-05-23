9e5fce5eaf3e73ce9cfa207237babba14e4c1030
Prachi Chavda <prachischavda1@gmail.com>
2017-05-23 15:25:05 +0200

    adding new tests

---
+++ tests/python/TestAxis.py
@@ -0,0 +1,15 @@
+import pynsx as nsx
+import numpy as np
+import ctypes as c
+import unittest
+
+class TestNiggliReduction(unittest.TestCase):
+
+        # I am not able to convert the class def from C++ to py
+        #class TestAxis(label(dtype = ), physical(dtype = bool)): 
+      pass
+    
+if __name__ == '__main__':
+    unittest.main()
+
+  
