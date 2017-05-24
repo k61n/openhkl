c40380eaaf9bac79e750930716ad1e3d0ed2ab56
Prachi Chavda <prachischavda1@gmail.com>
2017-05-24 17:10:44 +0200

    adding new test - 'TestConvexHull.py'

--- tests/python/TestConvexHull.py
+++ tests/python/TestConvexHull.py
@@ -3,9 +3,28 @@ import numpy as np
 import ctypes as c
 import unittest
 
-class TestNiggliReduction(unittest.TestCase):
-    pass
-        
+class TestComplexHull(unittest.TestCase):
+    def test(self):
+        # ! check that the Hull satisfies the convexity condition. This consists in
+        # ! checking that the signed volume between every face and every point is positive.
+        # ! This shows that each point is inside every face and therefore the hull is convex.
+
+        def checkConvexity(chull): # todo : check if it is correctly imported    
+            #todo : getFaces() is not imported to py 
+            faces = chull.getFaces()
+            vertices = chull.getVertices()
+
+            # todo : cannot use faces instance
+           # for f in faces
+            #    for v in vertices
+             #       if(f.volumeSign(v) < 0)
+              #          return False
+               # return True
+    
+    def Test_ConvexHull():
+        CHullDouble = ConvexHall(dtype = double)
+        # to be done ...
+
 
 if __name__ == '__main__':
     unittest.main()
