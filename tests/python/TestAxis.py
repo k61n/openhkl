f5567b974d62178782bcdb1b1a05f5aa85d81282
Prachi Chavda <prachischavda1@gmail.com>
2017-05-24 15:15:07 +0200

    adding new py test - TestBlobFinder.py- IT IS EMPTY_Not yet implemented

--- tests/python/TestAxis.py
+++ tests/python/TestAxis.py
@@ -3,11 +3,10 @@ import numpy as np
 import ctypes as c
 import unittest
 
-class TestNiggliReduction(unittest.TestCase):
-
-        # I am not able to convert the class def from C++ to py
-        #class TestAxis(label(dtype = ), physical(dtype = bool)): 
-      pass
+class TestAxis(unittest.TestCase):
+    
+    def test(self):
+        pass
     
 if __name__ == '__main__':
     unittest.main()
--- tests/python/TestBlobFinder.py
+++ tests/python/TestBlobFinder.py
@@ -3,9 +3,10 @@ import numpy as np
 import ctypes as c
 import unittest
 
-class TestNiggliReduction(unittest.TestCase):
-    pass
-        
+class TestBlobfinder(unittest.TestCase):
+    def test(self):
+        # todo : 'Test_BlobFinder' is not implemented
+        pass
 
 if __name__ == '__main__':
     unittest.main()
