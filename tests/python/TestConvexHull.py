6cab982f07391116b2acafa7023dfdce1257c382
Prachi Chavda <prachischavda1@gmail.com>
2017-05-29 12:41:07 +0200

    Adding new python tests- TestConvexHull.py

--- tests/python/TestConvexHull.py
+++ tests/python/TestConvexHull.py
@@ -23,7 +23,88 @@ class TestComplexHull(unittest.TestCase):
     
     def Test_ConvexHull():
         CHullDouble = ConvexHall(dtype = double)
-        # to be done ...
+       
+        # creates an empty convex HUll
+        chull = CHullDouble()
+        
+        # todo : check if this below line is actually is analog of its c++ counterpart
+        # Checks that the hull can not be updated with 0 point to be processed
+        self.assertAlmostEqual([chull.updateHull(),0])
+        
+        # todo : check if its working - "chull.addVertex(vec1)", c++ code has "std::runtime_error"
+        # todo : what is the analog of std::runtime_error to python
+        # Fill it with three vertices to form a tetrahedron
+        vec1 = np.array([0,0,0])
+        chull.addVertex(vec1)
+        # Checks that the hull can not be updated with only 1 point to be processed
+        self.assertAlmostEqual([chull.updateHull(),0])
+                    
+        # Fill it with three vertices to form a tetrahedron
+        vec2 =  np.array([10,0,0])
+        chull.addVertex([vec2])
+        #Checks that the hull can not be updated with only 2 point to be processed
+        self.assertAlmostEqual([chull.updateHull(),0])
+
+        vec3 = np.array([0,10,0])
+        chull.addVertex([vec3])
+        #Checks that the hull can not be updated with only 3 point to be processed
+        self.assertAlmostEqual([0,0,10])
+
+        vec4 = np.array([0,0,10])
+        chull.addVertex([vec3])
+        # Checks that with 4 vertices the hull can be built
+        self.assertAlmostEqual([chull.updateHull(),0])
+
+        faces = chull.getFaces()
+        edges = chull.getEdges()
+        vertices = chull.getVertices()
+
+        #  todo : not working - BOOST_CHECK_EQUAL(vertices.size(),4);
+        self.assertAlmostEqual([vertices.size(),4])
+        self.assertAlmostEqual([edges.size(),6])
+        self.assertAlmostEqual([faces.size(),4])
+        
+        # ! Checks that the hull satisfies the Euler conditions
+        # todo : check if assertAlmostEqual is right conversion here
+        self.assertAlmostEqual(checkEulerConditions())
+
+        # ! Checks that the hull satisfies the Convexity condition
+        self.assertAlmostEqual(checkConvexity(chull))
+        
+        # Fill the convex hull with new vertices in order to make a cube
+        chull.addVertex(np.array([10,10,0]))
+        chull.addVertex(np.array([10,0,10]))
+        chull.addVertex(np.array([0,10,10]))
+        chull.addVertex(np.array([10,10,10]))
+
+        # update the hull
+        chull.updateHull()
+
+        # Check that the number of vertices, edges and faces corresponds to a cube
+        self.assertAlmostEqual([vertices.size(),8])
+        self.assertAlmostEqual([edges.size(),18])
+        self.assertAlmostEqual([faces.size(),12])
+        
+        # ! Checks that the hull satisfies the Euler conditions
+        self.assertAlmostEqual(checkEulerConditions())
+
+        # ! Checks that the hull satisfies the Convexity condition
+        self.assertAlmostEqual(checkConvexity(chull))
+        
+        # ! Checks that the volume of the cube is 10*10*10=1000
+        self.assertAlmostEqual([chull.getVolume(),1000,0])
+
+        oldVolume = chull.getVolume() 
+        chull.translateToCenter()
+        newVolume = chull.getVolume()
+        self.assertAlmostEqual([oldVolume,newVolume,0])
+
+        # Check that the copy construction is OK
+        newHull(CHullDouble, chull) 
+        self.assertAlmostEqual([chull.getNVertices(),newHull.getNVertices()],0)
+        self.assertAlmostEqual([chull.getNEdges(),newHull.getNEdges()],0)
+        self.assertAlmostEqual([chull.getNFaces(),newHull.getNFaces()],0)
+        self.assertAlmostEqual([chull.getNVolume(),newHull.getNVolume()],0)
 
 
 if __name__ == '__main__':
