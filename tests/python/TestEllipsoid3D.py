e741dad430a1ffb96e94cbb96337bb6791affff4
Prachi Chavda <prachischavda1@gmail.com>
2017-05-30 12:35:57 +0200

    adding new test - 'TestEllipsoid3D.py'

--- tests/python/TestEllipsoid3D.py
+++ tests/python/TestEllipsoid3D.py
@@ -3,9 +3,17 @@ import numpy as np
 import ctypes as c
 import unittest
 
-class TestNiggliReduction(unittest.TestCase):
-    pass
+class TestEllipsoid3d(unittest.TestCase):
+    def test(self):
+        center = np.array([10,10,10])
+        semi_axes = np.array([3,3,4])
+        eigV = np.array([(1,0,0),(0,1,0),(0,0,1)])
         
+        # todo: Ellipsoid is not defined
+        #e = Ellipsoid(center,semi_axes,eigV)
+        #e.translate(np.array([1,0,0]))
+
+
 
 if __name__ == '__main__':
     unittest.main()
