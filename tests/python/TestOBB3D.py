e2afce46f4c3f420d2a4d682c424b8ecc9370ce5
Prachi Chavda <prachischavda1@gmail.com>
2017-06-06 16:25:57 +0200

    Adding new test

--- tests/python/TestOBB3D.py
+++ tests/python/TestOBB3D.py
@@ -3,9 +3,18 @@ import numpy as np
 import ctypes as c
 import unittest
 
-class TestNiggliReduction(unittest.TestCase):
-    pass
-        
+class TestOBB3D(unittest.TestCase):
+    tolerance = 1e-5
+    tolerance_large = 1.0
+    def test(self):
+        # todo : test the construction of an OBB
+        center = np.array([3,4,7]) 
+        extent = np.array([np.sqrt(2),np.sqrt(2),1])   
+        axis = np.array([(1,0,0),(0,1,0),(0,0,1)])
+        # todo : nsx has no atribute named OBB
+        obb1 = nsx.OBB(center,extent,axis)
+    
+
 
 if __name__ == '__main__':
     unittest.main()
