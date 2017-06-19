9933e621772d3ff39acddb484374a7eb69976695
Prachi Chavda <prachischavda1@gmail.com>
2017-06-19 15:29:20 +0200

    new tests

---
+++ tests/python/TestSpaceGroupCSV.py
@@ -0,0 +1,20 @@
+import pynsx as nsx
+import numpy as np
+import ctypes as c
+import unittest
+
+class TestSpaceGroupCSV(unittest.TestCase):
+
+    def test(self):
+        table = nsx.SpaceGroupSymbols()
+        num_rows = 0
+
+        counts = map
+        symbols = table.getAllSymbols()
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+  
