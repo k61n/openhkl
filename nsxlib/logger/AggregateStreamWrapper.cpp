28c0a0a322f8af909e5231d1f98a5c8555676a0b
Eric Pellegrini <pellegrini@ill.fr>
2017-08-28 17:01:49 +0200

    added AggregateStreamWrapper for log in message simultaneously on different stream

---
+++ nsxlib/logger/AggregateStreamWrapper.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+
+#include "AggregateStreamWrapper.h"
+
+namespace nsx {
+
+AggregateStreamWrapper::AggregateStreamWrapper() : _wrappers(), IStreamWrapper() {
+}
+
+AggregateStreamWrapper::~AggregateStreamWrapper()
+{
+    for (auto w : _wrappers)
+        delete w;
+    _wrappers.clear();
+}
+
+void AggregateStreamWrapper::print(const std::string& message)
+{
+    for (auto w : _wrappers) {
+        w->print(message);
+    }
+}
+
+void AggregateStreamWrapper::addWrapper(IStreamWrapper* wrapper)
+{
+    auto it = std::find(_wrappers.begin(),_wrappers.end(),wrapper);
+
+    if (it == _wrappers.end()) {
+        _wrappers.emplace_back(wrapper);
+    }
+}
+
+} /* namespace nsx */
