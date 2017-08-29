11756cf5876756265d5827979bfd630bc287c1d1
Eric Pellegrini <pellegrini@ill.fr>
2017-08-29 09:38:01 +0200

    added QtStreamWrapper to wrap Qt debugging channel for nsxqt

---
+++ apps/nsxqt/logger/QtStreamWrapper.cpp
@@ -0,0 +1,21 @@
+/*
+ * QtStreamWrapper.cpp
+ *
+ *  Created on: Aug 29, 2017
+ *      Author: pellegrini
+ */
+
+#include <logger/QtStreamWrapper.h>
+
+namespace nsx {
+
+QtStreamWrapper::QtStreamWrapper() {
+    // TODO Auto-generated constructor stub
+
+}
+
+QtStreamWrapper::~QtStreamWrapper() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace nsx */
---
+++ apps/nsxqt/logger/QtStreamWrapper.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace nsx {
+
+class QtStreamWrapper {
+public:
+    QtStreamWrapper();
+    virtual ~QtStreamWrapper();
+};
+
+} /* namespace nsx */
