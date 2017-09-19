7e14740adf11d6d576e3734206b817c95b4f1639
Eric Pellegrini <pellegrini@ill.fr>
2017-09-19 08:59:54 +0200

    added unit test for NSX_CHECK_ASSERT

--- tests/utils/TestNSXTest.cpp
+++ tests/utils/TestNSXTest.cpp
@@ -23,28 +23,34 @@ int main() {
         return 1;
     }
 
+    NSX_TEST_CHECK_ASSERT(false);
+    if (all_tests.nSuccesses() != 1 || all_tests.nFailures() != 1 || all_tests.nTests() != ++n_tests) {
+        std::cout<<"Error when performing nsx unit test framework"<<std::endl;
+        return 1;
+    }
+
     // Check integer equality/inequality
 
     NSX_TEST_CHECK_EQUAL(1,1);
-    if (all_tests.nSuccesses() != 2 || all_tests.nFailures() != 0 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 2 || all_tests.nFailures() != 1 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
     NSX_TEST_CHECK_EQUAL(1l,1l);
-    if (all_tests.nSuccesses() != 3 || all_tests.nFailures() != 0 || all_tests.nTests() != ++n_tests) {
-        std::cout<<"Error when performing nsx unit test framework"<<std::endl;
-        return 1;
-    }
-
-    NSX_TEST_CHECK_EQUAL(1,2);
     if (all_tests.nSuccesses() != 3 || all_tests.nFailures() != 1 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
+    NSX_TEST_CHECK_EQUAL(1,2);
+    if (all_tests.nSuccesses() != 3 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+        std::cout<<"Error when performing nsx unit test framework"<<std::endl;
+        return 1;
+    }
+
     NSX_TEST_CHECK_NOT_EQUAL(1,2);
-    if (all_tests.nSuccesses() != 4 || all_tests.nFailures() != 1 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 4 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
@@ -52,25 +58,25 @@ int main() {
     // Check floatings equality/inequality within a given tolerance
 
     NSX_TEST_CHECK_CLOSE(1.0,1.1,0.5);
-    if (all_tests.nSuccesses() != 5 || all_tests.nFailures() != 1 || all_tests.nTests() != ++n_tests) {
-        std::cout<<"Error when performing nsx unit test framework"<<std::endl;
-        return 1;
-    }
-
-    NSX_TEST_CHECK_CLOSE(1.0f,18.3f,0.2f);
     if (all_tests.nSuccesses() != 5 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
+    NSX_TEST_CHECK_CLOSE(1.0f,18.3f,0.2f);
+    if (all_tests.nSuccesses() != 5 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
+        std::cout<<"Error when performing nsx unit test framework"<<std::endl;
+        return 1;
+    }
+
     NSX_TEST_CHECK_NOT_CLOSE(1.0f,1.3f,0.2f);
-    if (all_tests.nSuccesses() != 6 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 6 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
     NSX_TEST_CHECK_NOT_CLOSE(1.0,2.0,0.1);
-    if (all_tests.nSuccesses() != 7 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 7 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
@@ -78,25 +84,25 @@ int main() {
     // Check exception throwed
 
     NSX_TEST_CHECK_EXCEPTION(throw std::runtime_error("I am failing"),std::runtime_error);
-    if (all_tests.nSuccesses() != 8 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 8 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
     NSX_TEST_CHECK_EXCEPTION(throw MyException(),MyException);
-    if (all_tests.nSuccesses() != 9 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 9 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
     NSX_TEST_CHECK_EXCEPTION(throw MyException(),std::exception);
-    if (all_tests.nSuccesses() != 10 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 10 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
 
     NSX_TEST_CHECK_ANY_EXCEPTION(throw MyException());
-    if (all_tests.nSuccesses() != 11 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 11 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
@@ -104,7 +110,7 @@ int main() {
     // Check no exception throwed
 
     NSX_TEST_CHECK_NO_EXCEPTION(true);
-    if (all_tests.nSuccesses() != 12 || all_tests.nFailures() != 2 || all_tests.nTests() != ++n_tests) {
+    if (all_tests.nSuccesses() != 12 || all_tests.nFailures() != 3 || all_tests.nTests() != ++n_tests) {
         std::cout<<"Error when performing nsx unit test framework"<<std::endl;
         return 1;
     }
