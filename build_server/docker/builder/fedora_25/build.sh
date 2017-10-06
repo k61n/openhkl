f964ba3e887facf634dd2227b3091785a1ad9448
Eric Pellegrini <pellegrini@ill.fr>
2017-10-06 13:28:19 +0200

    added docker files for fedora 25

---
+++ build_server/docker/builder/fedora_25/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# build the nsxtool image
+docker build --force-rm -t nsxtool .
+
+# run the nsxtool container
+id=`docker run -d -t nsxtool /bin/sh`
+
+# cp the build artefact from the container to host
+#docker cp $id:/tmp/nsxtool_1.0.0-1_amd64.deb .
+
+# kill the nsxtool daemon container
+docker kill $id
+
+# remove the nsxtool container
+docker rm $id
+
+# remove nsxtool image and associated dangling images
+docker rmi nsxtool
+
