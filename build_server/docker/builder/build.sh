31458e0ccde67658488c6e28fc0ae0c4ee9dfc02
Eric Pellegrini <pellegrini@ill.fr>
2017-10-17 10:47:56 +0200

    solved missing deps for several distro setup docker files

--- build_server/docker/builder/build.sh
+++ build_server/docker/builder/build.sh
@@ -4,7 +4,7 @@
 docker build --force-rm --build-arg NSX_GIT_BRANCH=$1 -t nsxtool -f $2/Dockerfile .
 
 # run the nsxtool container
-id=`docker run -d -t nsxtool`
+id=`docker run -d -t nsxtool /bin/bash`
 
 # cp the build artefact from the container to host
 docker cp $id:/tmp/build-artefacts.tar.gz $2/.
