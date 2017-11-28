b7d4d8a37cb15740af86a1e116e98e1b4d931672
Eric Pellegrini <pellegrini@ill.fr>
2017-11-28 11:53:09 +0100

    updated the hardcoded python path for macos builder

--- build_server/osx/run_build.sh
+++ build_server/osx/run_build.sh
@@ -4,6 +4,6 @@ cd ${CI_PROJECT_DIR}
 mkdir ./build
 cd build
 
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_QTAPPS=ON -DCMAKE_PREFIX_PATH="/usr/local/Cellar/python/2.7.13/Frameworks/Python.framework/Versions/2.7;/usr/local/Cellar/qt5/5.6.0;/usr/local/Cellar/gsl/2.3" -DCMAKE_INSTALL_PREFIX=. -DNSX_PYTHON=ON -DNSX_PYTHON3=OFF ..
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_QTAPPS=ON -DCMAKE_PREFIX_PATH="/Library/Frameworks/Python.framework/Versions/2.7;/usr/local/Cellar/qt5/5.6.0;/usr/local/Cellar/gsl/2.3" -DCMAKE_INSTALL_PREFIX=. -DNSX_PYTHON=ON -DNSX_PYTHON3=OFF ..
 cmake --build . --config Release
 cmake --build . --config Release --target install
