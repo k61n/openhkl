527331f2de795bc33f0c60a2e3efe4b842fb68db
Eric Pellegrini <pellegrini@ill.fr>
2018-03-23 10:34:13 +0100

    Removed Convolver class and unit test

--- tests/peak_find/TestDeltaKernel.cpp
+++ tests/peak_find/TestDeltaKernel.cpp
@@ -1,7 +1,34 @@
+#include <nsxlib/DeltaKernel.h>
+#include <nsxlib/MathematicsTypes.h>
 #include <nsxlib/NSXTest.h>
+
+// Generate a image to use for testing the Fourier transform
+nsx::RealMatrix generateImage(int rows, int cols)
+{
+    nsx::RealMatrix image(rows, cols);
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            image(i, j) = i*i + i - 2*j - j*j; // something NOT symmetric in i or j, and i<->j
+        }
+    }
+    return image;
+}
+
+double computeError(int nrows, int ncols)
+{
+    nsx::RealMatrix original = generateImage(nrows,ncols);
+    nsx::DeltaKernel convolver;
+
+    nsx::RealMatrix transformed = convolver.convolve(original);
+    nsx::RealMatrix difference =  original-transformed;
+    double error = (difference * difference.transpose()).sum() / nrows / ncols;
+    return std::sqrt(error);
+}
 
 int main()
 {
-#pragma message "test not implemented"
+    double error = computeError(30, 20);
+    NSX_CHECK_ASSERT(error < 1e-10);
 }
 
