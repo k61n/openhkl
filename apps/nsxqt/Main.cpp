f749a4d8b04530c2794e8cc05835793743605b01
jmfisher <j.fisher@fz-juelich.de>
2018-04-18 09:31:33 +0200

    add qt version check for high DPI

--- apps/nsxqt/Main.cpp
+++ apps/nsxqt/Main.cpp
@@ -15,7 +15,9 @@
 
 int main(int argc, char* argv[])
 {
+    #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
     QGuiApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+    #endif
     NSXQtApp a(argc, argv);
 
     // Ensure . is used rather than , for float and double boxes
