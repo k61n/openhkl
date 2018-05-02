d5e75cc15afe62aa67caa4fb248b2a57766d8ed5
Eric Pellegrini <pellegrini@ill.fr>
2018-05-02 19:20:28 +0200

    cosmetic changes in run_deploy.bat

--- build_server/windows/run_deploy.bat
+++ build_server/windows/run_deploy.bat
@@ -2,16 +2,16 @@ set PROJECT_DIR=%cd%
 
 set BUILD_DIR=%PROJECT_DIR%\build
 
-C:\Qt\5.6.1\msvc2017_64\bin\windeployqt %BUILD_DIR%\build\bin\nsxqt.exe
+C:\Qt\Qt5.6.1\5.6\msvc2015_64\bin\windeployqt %BUILD_DIR%\bin\nsxqt.exe
 
-copy %BUILD_DIR%\externals\c-blosc\blosc\Release\blosc.dll %BUILD_DIR%\build\bin\.
+copy %BUILD_DIR%\externals\c-blosc\blosc\blosc.dll %BUILD_DIR%\bin\.
 
-copy C:\opt\bin\fftw3.dll %BUILD_DIR%\build\bin\.
+copy C:\opt\local_x64\bin\fftw3.dll %BUILD_DIR%\bin\.
 
-copy C:\opt\bin\tiff.dll %BUILD_DIR%\build\bin\.
+copy C:\opt\local_x64\bin\tiff.dll %BUILD_DIR%\bin\.
 
-copy C:\opt\bin\zlib.dll %BUILD_DIR%\build\bin\.
-copy C:\opt\bin\hdf5.dll %BUILD_DIR%\build\bin\.
-copy C:\opt\bin\hdf5_cpp.dll %BUILD_DIR%\build\bin\.
+copy C:\opt\local_x64\bin\zlib.dll %BUILD_DIR%\bin\.
+copy C:\opt\local_x64\bin\hdf5.dll %BUILD_DIR%\bin\.
+copy C:\opt\local_x64\bin\hdf5_cpp.dll %BUILD_DIR%\bin\.
 
 makensis /V4 /DVERSION=1.0.0 /DTARGET_DIR=%BUILD_DIR% /DARCH="win-amd64" %PROJECT_DIR%\build_server\windows\build_nsxtool_installer.nsi
