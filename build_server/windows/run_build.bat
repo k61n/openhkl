ab9ee77b3cefd7077ee920dc0b6b5f5a8b6181a6
Eric Pellegrini <pellegrini@ill.fr>
2018-05-02 19:35:34 +0200

    test windows ci

--- build_server/windows/run_build.bat
+++ build_server/windows/run_build.bat
@@ -2,7 +2,7 @@ rmdir /s /q build
 mkdir build
 cd build
 
-set BOOST_ROOT=c:\opt\local_x64
+set BOOST_ROOT=C:\opt\local_x64
 set FFTW_DIR=C:\opt\local_x64
 set GSL_ROOT_DIR=C:\opt\local_x64
 set HDF5_ROOT=C:\opt\local_x64
@@ -10,6 +10,8 @@ set HDF5_ROOT=C:\opt\local_x64
 set PATH=C:\opt\local_x64\bin;%PATH%
 
 call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat" amd64
+
+set
 
 cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QTDIR%\lib\cmake -DYAML_ROOT_DIR=C:\opt\local_x64 -DTIFF_ROOT=C:\opt\local_x64 -DZLIB_ROOT=C:\opt\local_x64 -DNSX_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=.
 cmake --build . --config Release
