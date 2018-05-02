88dd9664fb88633d0f7c21290969d5320b4c4f75
Eric Pellegrini <pellegrini@ill.fr>
2018-05-02 21:08:24 +0200

    remove debug print

--- build_server/windows/run_build.bat
+++ build_server/windows/run_build.bat
@@ -9,11 +9,7 @@ set HDF5_ROOT=C:\opt\local_x64
 
 set PATH=C:\opt\local_x64\bin;%PATH%
 
-set
-
 call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat" amd64
-
-set
 
 cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QTDIR%\lib\cmake -DYAML_ROOT_DIR=C:\opt\local_x64 -DTIFF_ROOT=C:\opt\local_x64 -DZLIB_ROOT=C:\opt\local_x64 -DNSX_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=.
 cmake --build . --config Release
