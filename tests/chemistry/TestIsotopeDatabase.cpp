9db54558a72068595c5e55fa0d3a4f5f6a7be807
Eric Pellegrini <pellegrini@ill.fr>
2018-06-13 08:48:32 +0200

    Cleaned up Material and IsosopteDatabaseManager classes

--- tests/chemistry/TestIsotopeDatabase.cpp
+++ tests/chemistry/TestIsotopeDatabase.cpp
@@ -11,19 +11,19 @@ int main()
     nsx::IsotopeDatabaseManager* imgr = nsx::IsotopeDatabaseManager::Instance();
     nsx::UnitsManager* um = nsx::UnitsManager::Instance();
 
-    NSX_CHECK_EQUAL(imgr->getProperty<std::complex<double>>("Hf[176]","b_coherent"),std::complex<double>(6.61*1.0e-15,0));
+    NSX_CHECK_EQUAL(imgr->property<std::complex<double>>("Hf[176]","b_coherent"),std::complex<double>(6.61*1.0e-15,0));
 
     NSX_CHECK_ASSERT(imgr->hasProperty("Hf[176]","nuclear_spin"));
 
     NSX_CHECK_ASSERT(!imgr->hasProperty("Hf[176]","xxxxxx"));
 
     // Checks some of the property of the isotope
-    NSX_CHECK_CLOSE(imgr->getProperty<double>("H[1]","molar_mass"),1.00782504*um->get("g_per_mole"),tolerance);
-    NSX_CHECK_EQUAL(imgr->getProperty<int>("H[1]","n_protons"),1);
-    NSX_CHECK_ASSERT(imgr->getProperty<bool>("H[1]","stable"));
-    NSX_CHECK_ASSERT(imgr->getProperty<std::string>("H[1]","symbol").compare("H")==0);
+    NSX_CHECK_CLOSE(imgr->property<double>("H[1]","molar_mass"),1.00782504*um->get("g_per_mole"),tolerance);
+    NSX_CHECK_EQUAL(imgr->property<int>("H[1]","n_protons"),1);
+    NSX_CHECK_ASSERT(imgr->property<bool>("H[1]","stable"));
+    NSX_CHECK_ASSERT(imgr->property<std::string>("H[1]","symbol").compare("H")==0);
 
-    NSX_CHECK_EQUAL(imgr->getProperty<int>("Ag[107]","n_neutrons"),60);
+    NSX_CHECK_EQUAL(imgr->property<int>("Ag[107]","n_neutrons"),60);
 
     return 0;
 }
