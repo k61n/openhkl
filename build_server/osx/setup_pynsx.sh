00c97ebf6822cb2601d9ff62805bc534750bc5f8
Eric Pellegrini <pellegrini@ill.fr>
2018-06-19 17:42:22 +0200

    Added CI variable to setup_pynsx.sh

--- build_server/osx/setup_pynsx.sh
+++ build_server/osx/setup_pynsx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NSX_APP_CONTENTS=/Users/ci/temp/nsxtool/build/qmake-build/nsxtool.app/Contents
+NSX_APP_CONTENTS=${CI_PROJECT_DIR}/build/qmake-build/nsxtool.app/Contents
 
 # Setup the python to be used with pynsx
 
@@ -20,8 +20,8 @@ mkdir ${NSX_APP_CONTENTS}/lib/python2.7/site-packages
 cp -r /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/numpy ${NSX_APP_CONTENTS}/lib/python2.7/site-packages
 
 # Copy pynsx.py and _pynsx.so in nsxtool app
-cp /Users/ci/temp/nsxtool/build/swig/_pynsx.so ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/.
-cp /Users/ci/temp/nsxtool/build/swig/pynsx.py ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/.
+cp ${CI_PROJECT_DIR}/build/swig/_pynsx.so ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/.
+cp ${CI_PROJECT_DIR}/build/swig/pynsx.py ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/.
 
 # Modify the library dynamic path to make python self-contained
 install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @executable_path/../Frameworks/libpython2.7.dylib ${NSX_APP_CONTENTS}/MacOS/python
@@ -31,7 +31,7 @@ install_name_tool -id @executable_path/../Frameworks/libpython2.7.dylib ${NSX_AP
 install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @executable_path/../Frameworks/libpython2.7.dylib ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/_pynsx.so
 
 # Modify the path to nsx library in _pynsx binding
-install_name_tool -change /Users/ci/temp/nsxtool/build/nsxlib/libnsx.dylib @executable_path/../Frameworks/libnsx.dylib ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/_pynsx.so
+install_name_tool -change ${CI_PROJECT_DIR}/build/nsxlib/libnsx.dylib @executable_path/../Frameworks/libnsx.dylib ${NSX_APP_CONTENTS}/lib/python2.7/site-packages/_pynsx.so
 
 # Modify the path to libz for HDF5 and Tiff dylibs
 install_name_tool -change /usr/lib/libz.1.dylib @executable_path/../Frameworks/libz.1.dylib ${NSX_APP_CONTENTS}/Frameworks/libhdf5.101.dylib
