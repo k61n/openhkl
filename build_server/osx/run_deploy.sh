c08c7d7e246cc42efe25165ec28f213514f95066
Eric Pellegrini <pellegrini@ill.fr>
2018-06-28 10:38:19 +0200

    Set ENABLE_CPACK to ON in MacOS run_deploy script

--- build_server/osx/run_deploy.sh
+++ build_server/osx/run_deploy.sh
@@ -7,6 +7,6 @@ cd ${CI_PROJECT_DIR}
 mkdir build
 cd build
 
-cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DNSX_PYTHON=ON -DBUILD_NSX_APPS=ON -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH="${QTDIR};/usr/local/Cellar/gsl/2.3" ..
+cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DNSX_PYTHON=ON -DBUILD_NSX_APPS=ON -DENABLE_CPACK=ON -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH="${QTDIR};/usr/local/Cellar/gsl/2.3" ..
 
 cmake --build . --config Release --target package
