84c0171af5a2a8d70d6da24daf183996701bdad0
Eric Pellegrini <pellegrini@ill.fr>
2018-09-17 13:15:03 +0200

    Added an item delagate class for session model

---
+++ apps/nsxqt/models/SessionModelDelegate.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QStyledItemDelegate>
+
+class SessionModelDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    SessionModelDelegate();
+
+    virtual void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+
+    virtual bool editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index);
+};
