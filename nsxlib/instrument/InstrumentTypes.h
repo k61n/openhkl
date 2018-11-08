efbfe7c4d465f9a524b88a08b5a22546cbbea8d2
Eric Pellegrini <pellegrini@ill.fr>
2018-11-08 18:50:20 +0100

    Removed typedef for shared ptr to gonio

--- nsxlib/instrument/InstrumentTypes.h
+++ nsxlib/instrument/InstrumentTypes.h
@@ -19,7 +19,6 @@ class Sample;
 class Source;
 
 using sptrDetector       = std::shared_ptr<Detector>;
-using sptrGonio          = std::shared_ptr<Gonio>;
 using sptrMonoDetector   = std::shared_ptr<MonoDetector>;
 using sptrDiffractometer = std::shared_ptr<Diffractometer>;
 using sptrExperiment     = std::shared_ptr<Experiment>;
