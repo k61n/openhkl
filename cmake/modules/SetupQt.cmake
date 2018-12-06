388ef87ad5c179cd157aafb190b007199e5e15a9
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2018-12-06 20:21:06 +0100

    Require CMake version >= 3.6
    Will be needed for packaging anyway;
    saves us a lengthy case distinction in SetupQt.

--- cmake/modules/SetupQt.cmake
+++ cmake/modules/SetupQt.cmake
@@ -41,51 +41,13 @@ message(STATUS "  headers: ${Qt5Widgets_INCLUDE_DIRS}")
 
 set(QT_LIBRARIES ${Qt5Core_LIBRARIES} ${Qt5Gui_LIBRARIES} ${Qt5OpenGL_LIBRARIES} ${Qt5PrintSupport_LIBRARIES} ${Qt5Widgets_LIBRARIES})
 
-if (${CMAKE_VERSION} VERSION_LESS 2.8.11)
-
-  # Add the include directories for the Qt 5 Widgets module to the compile lines.
-  # There might be some redundancies.
-  include_directories(${Qt5Core_INCLUDE_DIRS})
-  include_directories(${Qt5PrintSupport_INCLUDE_DIRS})
-  include_directories(${Qt5OpenGL_INCLUDE_DIRS})
-  include_directories(${Qt5Widgets_INCLUDE_DIRS})
-  include_directories(${Qt5Gui_INCLUDE_DIRS})
-
-  # Use the compile definitions defined in the Qt 5 Widgets module
-  # There might be some redundancies.
-  add_definitions(${Qt5Core_DEFINITIONS})
-  add_definitions(${Qt5PrintSupport_DEFINITIONS})
-  add_definitions(${Qt5OpenGL_DEFINITIONS})
-  add_definitions(${Qt5Widgets_DEFINITIONS})
-  add_definitions(${Qt5Gui_DEFINITIONS})
-
-  # Add compiler flags for building executables (i.e. -fPIE)
-  # There might be some redundancies.
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5Core_EXECUTABLE_COMPILE_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5PrintSupport_EXECUTABLE_COMPILE_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5OpenGL_EXECUTABLE_COMPILE_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5Gui_EXECUTABLE_COMPILE_FLAGS}")
-
-  # Generate the Qt moc files
-  # All the sources files that are not Qt Q_OBJECT will trigger a warning
-  # and won't be compiled as moc objects.
-  qt5_wrap_cpp(NSXQT_MOC_CPP ${NSXQT_UI_HEADERS})
-
-  # "Compile" the resources files
-  qt5_add_resources(NSXQT_RESOURCES_CPP ${NSXQT_RCS})
-
-else()
-
-  # Find includes in corresponding build directories
-  set(CMAKE_INCLUDE_CURRENT_DIR ON)
-  # Handle automatically moc files
-  set(CMAKE_AUTOMOC ON)
-  # Handle automatically ui files
-  set(CMAKE_AUTOUIC OFF)
-  # Specify that the app will use OpenGL
-  set(QT_USE_QTOPENGL TRUE)
-  # Handle automatically resources files
-  set(CMAKE_AUTORCC ON)
-
-endif()
+# Find includes in corresponding build directories
+set(CMAKE_INCLUDE_CURRENT_DIR ON)
+# Handle automatically moc files
+set(CMAKE_AUTOMOC ON)
+# Handle automatically ui files
+set(CMAKE_AUTOUIC OFF)
+# Specify that the app will use OpenGL
+set(QT_USE_QTOPENGL TRUE)
+# Handle automatically resources files
+set(CMAKE_AUTORCC ON)
