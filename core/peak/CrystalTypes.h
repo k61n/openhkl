62591bb0d07dc791d1aa104ed549ad1b0ef13ea5
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2018-12-07 21:33:17 +0100

    rm unused includes and aliases from CrystalTypes

--- core/peak/CrystalTypes.h
+++ core/peak/CrystalTypes.h
@@ -2,29 +2,21 @@
 
 #include <memory>
 #include <set>
-#include <utility>
 #include <vector>
-
 #include <Eigen/Dense>
 
 namespace nsx {
 
 class MergedPeak;
-class Peak3D;
-class PeakIntegrator;
-class PeakValidator;
-class Profile;
-class UnitCell;
-
-using sptrPeak3D   = std::shared_ptr<Peak3D>;
-using sptrUnitCell = std::shared_ptr<UnitCell>;
-
-using RankedSolution = std::pair<sptrUnitCell,double>;
-
-using PeakList = std::vector<sptrPeak3D>;
-
 using MergedPeakSet = std::set<MergedPeak>;
 
+class Peak3D;
+using sptrPeak3D   = std::shared_ptr<Peak3D>;
+using PeakList = std::vector<sptrPeak3D>;
+
+class UnitCell;
+using sptrUnitCell = std::shared_ptr<UnitCell>;
+using RankedSolution = std::pair<sptrUnitCell,double>;
 using UnitCellList = std::vector<sptrUnitCell>;
 
 } // end namespace nsx
