3bb15a37c39eafa5be00521d37dd7e355cf6d9c7
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2018-12-07 15:07:59 +0100

    rm unused FilterCallback

--- core/mathematics/MathematicsTypes.h
+++ core/mathematics/MathematicsTypes.h
@@ -7,5 +7,4 @@ namespace nsx {
 using IntMatrix = Eigen::Matrix<int,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor>;
 using RealMatrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
-using FilterCallback = std::function<RealMatrix(const RealMatrix&)>;
 } // end namespace nsx
