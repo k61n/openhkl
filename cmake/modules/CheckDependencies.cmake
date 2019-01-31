79a12e3ad6380fbd6be84bbab9c0b2689596d453
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2019-01-31 09:22:05 +0100

    CMake: indentation

--- cmake/modules/CheckDependencies.cmake
+++ cmake/modules/CheckDependencies.cmake
@@ -15,9 +15,9 @@ endif()
 
 ###### Find OPENMP
 if (BUILD_WITH_OPENMP)
-  find_package(OpenMP REQUIRED)
-  set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-  set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+    find_package(OpenMP REQUIRED)
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
 endif()
 
 ###### Find the Eigen3
@@ -28,77 +28,77 @@ find_package(HDF5 COMPONENTS CXX REQUIRED)
 
 # manually fix problem with windows build
 if(NOT HDF5_LIBRARIES)
-  set(HDF5_LIBRARIES "")
-  find_library(HDF5_LIBRARY_TEMP libszip)
-  set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
+    set(HDF5_LIBRARIES "")
+    find_library(HDF5_LIBRARY_TEMP libszip)
+    set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
 
-  find_library(HDF5_LIBRARY_TEMP libzlib)
-  set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
+    find_library(HDF5_LIBRARY_TEMP libzlib)
+    set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
 
-  find_library(HDF5_LIBRARY_TEMP libhdf5)
-  set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
+    find_library(HDF5_LIBRARY_TEMP libhdf5)
+    set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
 
-  find_library(HDF5_LIBRARY_TEMP libhdf5_cpp)
-  set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
+    find_library(HDF5_LIBRARY_TEMP libhdf5_cpp)
+    set(HDF5_LIBRARIES "${HDF5_LIBRARIES};${HDF5_LIBRARY_TEMP}")
 endif()
 
 include_directories(SYSTEM "${HDF5_INCLUDE_DIRS}")
 
 if (HDF5_INCLUDE_DIRS AND HDF5_LIBRARIES)
-   message("HDF5 found:")
-   message("  versions: ${HDF5_VERSION}")
-   message("  libraries: ${HDF5_LIBRARIES}")
-   message("  headers: ${HDF5_INCLUDE_DIRS}")
+    message("HDF5 found:")
+    message("  versions: ${HDF5_VERSION}")
+    message("  libraries: ${HDF5_LIBRARIES}")
+    message("  headers: ${HDF5_INCLUDE_DIRS}")
 else()
-   message(FATAL_ERROR "HDF5 not found")
+    message(FATAL_ERROR "HDF5 not found")
 endif()
 
 if(NSX_PYTHON)
-  # python-dev and interpreter
-  if (USE_PYTHON3)
-    set(Python_ADDITIONAL_VERSIONS 3.7 3.6 3.5 3.4)
-    find_package(PythonInterp 3)
-    find_package(PythonLibs 3)
-  else()
-    set(Python_ADDITIONAL_VERSIONS 2.7)
-    find_package(PythonInterp 2)
-    find_package(PythonLibs 2)
-  endif()
+    # python-dev and interpreter
+    if (USE_PYTHON3)
+        set(Python_ADDITIONAL_VERSIONS 3.7 3.6 3.5 3.4)
+        find_package(PythonInterp 3)
+        find_package(PythonLibs 3)
+    else()
+        set(Python_ADDITIONAL_VERSIONS 2.7)
+        find_package(PythonInterp 2)
+        find_package(PythonLibs 2)
+    endif()
 
 
-  execute_process (
-    COMMAND ${PYTHON_EXECUTABLE} -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
+    execute_process (
+        COMMAND ${PYTHON_EXECUTABLE} -c "from __future__ import print_function; import numpy; print(numpy.get_include())"
         ERROR_VARIABLE NUMPY_FIND_ERROR
         RESULT_VARIABLE NUMPY_FIND_RESULT
         OUTPUT_VARIABLE NUMPY_FIND_OUTPUT
         OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
+        )
 
-  ## process the output from the execution of the command
-  if (NOT NUMPY_FIND_RESULT)
-    set (NUMPY_INCLUDES ${NUMPY_FIND_OUTPUT})
-    message(STATUS "numpy includes ${NUMPY_INCLUDES}")
-    include_directories(SYSTEM ${NUMPY_INCLUDES})
-  else()
-    message(FATAL_ERROR "Could NOT find numpy headers")
-  endif()
+    ## process the output from the execution of the command
+    if (NOT NUMPY_FIND_RESULT)
+        set (NUMPY_INCLUDES ${NUMPY_FIND_OUTPUT})
+        message(STATUS "numpy includes ${NUMPY_INCLUDES}")
+        include_directories(SYSTEM ${NUMPY_INCLUDES})
+    else()
+        message(FATAL_ERROR "Could NOT find numpy headers")
+    endif()
 
-  # Python packages dir
-  execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
-    "from __future__ import print_function; from distutils import sysconfig as sc; print(sc.get_python_lib(prefix='', plat_specific=True))"
-    RESULT_VARIABLE PYTHON_SITE_RESULT
-    OUTPUT_VARIABLE PYTHON_SITE
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # Python packages dir
+    execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
+        "from __future__ import print_function; from distutils import sysconfig as sc; print(sc.get_python_lib(prefix='', plat_specific=True))"
+        RESULT_VARIABLE PYTHON_SITE_RESULT
+        OUTPUT_VARIABLE PYTHON_SITE
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  if (NOT PYTHON_SITE_RESULT)
-    message(STATUS "python package destination is ${PYTHON_SITE}")
-  else()
-    message(FATAL_ERROR "could NOT determine python package directory")
-  endif()
+    if (NOT PYTHON_SITE_RESULT)
+        message(STATUS "python package destination is ${PYTHON_SITE}")
+    else()
+        message(FATAL_ERROR "could NOT determine python package directory")
+    endif()
 
-  # swig
-  find_package(SWIG REQUIRED)
-  include(UseSWIG)
+    # swig
+    find_package(SWIG REQUIRED)
+    include(UseSWIG)
 endif(NSX_PYTHON)
 
 ###### C-BLOSC
--- cmake/modules/CheckLinuxDistro.cmake
+++ cmake/modules/CheckLinuxDistro.cmake
@@ -1,17 +1,17 @@
 find_program(LSB_RELEASE_CMD lsb_release)
 
 if (NOT LSB_RELEASE_CMD)
-  message(FATAL_ERROR "lsb_release not found")
+    message(FATAL_ERROR "lsb_release not found")
 endif()
 
 execute_process(COMMAND ${LSB_RELEASE_CMD} -is OUTPUT_VARIABLE LINUX_DISTRO RESULT_VARIABLE result OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if ("${LINUX_DISTRO}" MATCHES "RedHatEnterprise" 
- OR "${LINUX_DISTRO}" MATCHES "Fedora" 
- OR "${LINUX_DISTRO}" MATCHES "Scientific"
- OR "${LINUX_DISTRO}" MATCHES "SUSE LINUX"
- OR "${LINUX_DISTRO}" MATCHES "openSUSE project"
- OR "${LINUX_DISTRO}" MATCHES "CentOS")
+if ("${LINUX_DISTRO}" MATCHES "RedHatEnterprise"
+        OR "${LINUX_DISTRO}" MATCHES "Fedora"
+        OR "${LINUX_DISTRO}" MATCHES "Scientific"
+        OR "${LINUX_DISTRO}" MATCHES "SUSE LINUX"
+        OR "${LINUX_DISTRO}" MATCHES "openSUSE project"
+        OR "${LINUX_DISTRO}" MATCHES "CentOS")
     set(DEBIAN_LIKE false)
 else()
     set(DEBIAN_LIKE true)
@@ -19,7 +19,7 @@ endif()
 
 # command failed
 if (result)
-  message(WARNING "Could not determine Linux distribution")
+    message(WARNING "Could not determine Linux distribution")
 else()
-  message(STATUS "The Linux distribution is ${LINUX_DISTRO}")
+    message(STATUS "The Linux distribution is ${LINUX_DISTRO}")
 endif()
