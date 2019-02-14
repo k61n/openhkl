953351477aa18e4da6fb5c2a8e370601100b2b3a
Alexander Schober <alex.schober@mac.com>
2019-02-14 09:25:29 +0100

    fix

---
+++ core/detector/DataOrder.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace nsx {
+
+enum class DataOrder : size_t {
+    TopLeftColMajor = 0,
+    TopLeftRowMajor = 1,
+    TopRightColMajor = 2,
+    TopRightRowMajor = 3,
+    BottomLeftColMajor = 4,
+    BottomLeftRowMajor = 5,
+    BottomRightColMajor = 6,
+    BottomRightRowMajor = 7
+};
+
+} // end namespace nsx
