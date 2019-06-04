5c70b81d9a08fd8800e07827baf2dd5059b79439
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2019-06-04 17:09:43 +0200

    add test of user-defined separators

--- test/cpp/utils/TestCSV.cpp
+++ test/cpp/utils/TestCSV.cpp
@@ -1,17 +1,20 @@
 #include "test/cpp/catch.hpp"
+
+#include <core/utils/CSV.h>
+
 #include <sstream>
 #include <string>
 #include <vector>
 
-#include "core/utils/CSV.h"
-TEST_CASE("test/utils/TestCSV.cpp", "") {
+// Test the CSV parser, which in turn seems to be needed for testing only.
+
+TEST_CASE("test/utils/TestCSV-default_separators", "") {
 
     std::string csv_file =
-        //    ",,,\n"
-        //    "a, b, c, d\n"
-        //    "e, f, g, h\n"
-        //    "1, 2, 3, 4\n"
-        "\"\", \"\"\"\"\", \"as,df\", \" \"\"\" \"";
+        ",,,\n"
+        "a, b, c, d\n"
+        "\"\", \"\"\"\"\", \"as,df\", \" \"\"\" \"\n" // test whether characters " and , are escaped
+        "e, f, g, h";
 
     std::istringstream stream(csv_file);
     nsx::CSV csv;
@@ -21,4 +24,24 @@ TEST_CASE("test/utils/TestCSV.cpp", "") {
         row = csv.getRow(stream);
         CHECK(row.size() == 4);
     }
+    std::cout << "Test 1 ok\n\n";
+}
+
+TEST_CASE("test/utils/TestCSV-user_defined_separators", "") {
+
+    std::string csv_file =
+        "a\tb\tc\td\n"
+        "a\tb\tc\t'd\te'\n"
+        "a\tb\tc\td'\t'f";
+
+    std::istringstream stream(csv_file);
+    nsx::CSV csv('\t', '\'');
+    std::vector<std::string> row;
+
+    while (!stream.eof()) {
+        std::cout << "New line:\n";
+        row = csv.getRow(stream);
+        CHECK(row.size() == 4);
+    }
+    std::cout << "Test 2 ok\n\n";
 }
