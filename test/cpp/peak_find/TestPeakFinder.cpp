cb7bfe94140c39e86b3abc6237352953ed227444
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2019-06-13 10:49:03 +0200

    rm unused includes

--- test/cpp/peak_find/TestPeakFinder.cpp
+++ test/cpp/peak_find/TestPeakFinder.cpp
@@ -1,8 +1,5 @@
 #include "test/cpp/catch.hpp"
 
-#include <Eigen/Dense>
-#include <vector>
-
 #include "core/experiment/DataSet.h"
 #include "core/experiment/Experiment.h"
 #include "core/instrument/Diffractometer.h"
@@ -14,29 +11,25 @@
 
 TEST_CASE("test/peak_find/TestPeakFinder.cpp", "")
 {
-
     nsx::Experiment experiment("exp", "D10");
-
     nsx::DataList numors;
 
     nsx::DataReaderFactory factory;
     auto dataf = factory.create("", "D10_ascii_example", experiment.diffractometer());
     const auto& metadata = dataf->reader()->metadata();
-    nsx::PeakFinder peakFinder;
     nsx::sptrProgressHandler handler(new nsx::ProgressHandler);
 
     CHECK(metadata.key<int>("nbang") == 2);
 
     dataf->open();
     numors.push_back(dataf);
-    peakFinder.setHandler(handler);
 
+    nsx::PeakFinder peakFinder;
+    peakFinder.setHandler(handler);
     peakFinder.setPeakScale(1.0);
 
     auto found_peaks = peakFinder.find(numors);
-    size_t num_peaks = found_peaks.size();
-
-    CHECK(num_peaks == 1);
+    CHECK(found_peaks.size() == 1);
 
     dataf->close();
 }
