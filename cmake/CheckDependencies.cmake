d51ed404d5cd5cf562fbef80d746ec9da750eafa
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2019-06-21 11:33:06 +0200

    CI: give Centos another chance with Boost 1.53 and clang

--- cmake/CheckDependencies.cmake
+++ cmake/CheckDependencies.cmake
@@ -1,6 +1,6 @@
 ###### Find boost
 set(Boost_USE_MULTITHREADED ON)
-find_package(Boost 1.54.0 REQUIRED)
+find_package(Boost 1.53.0 REQUIRED)
 if(Boost_FOUND)
     include_directories(SYSTEM "${Boost_INCLUDE_DIRS}")
     message(STATUS "Found boost:")
