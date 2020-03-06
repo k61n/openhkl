3a3237df63670827a325257bd3bb2b127802d141
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2020-03-06 15:30:18 +0100

    Insert standard headers in Python tests

--- test/python/TestMergedPeak.py
+++ test/python/TestMergedPeak.py
@@ -1,3 +1,17 @@
+##  ***********************************************************************************************
+##
+##  NSXTool: data reduction for neutron single-crystal diffraction
+##
+##! @file      test/python/TestMergedPeak.py
+##! @brief     Test ...
+##!
+##! @homepage  ###HOMEPAGE###
+##! @license   GNU General Public License v3 or higher (see COPYING)
+##! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+##! @authors   see CITATION, MAINTAINER
+##
+##  ***********************************************************************************************
+
 import pynsx as nsx
 import numpy as np
 import unittest
