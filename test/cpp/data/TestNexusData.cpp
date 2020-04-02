2b5ecba09b296db7e6ffb2c6534bfee8bb1f1c0c
Tobias Weber <tweber@ill.fr>
2020-04-02 14:11:57 +0200

    Nexus: updated loader to handle incomplete scans

--- test/cpp/data/TestNexusData.cpp
+++ test/cpp/data/TestNexusData.cpp
@@ -13,7 +13,11 @@
 //  ***********************************************************************************************
 
 #include "test/cpp/catch.hpp"
+
 #include <Eigen/Dense>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 #include "core/algo/DataReaderFactory.h"
 #include "core/data/DataSet.h"
@@ -24,21 +28,41 @@ TEST_CASE("test/data/TestNexusData.cpp", "")
 {
     nsx::DataReaderFactory factory;
 
+    std::vector<std::string> files = {
+        "/home/tw/tmp/nsx/internal-192/501168.nxs", "/home/tw/tmp/nsx/internal-192/501169.nxs",
+        "/home/tw/tmp/nsx/internal-192/501170.nxs", "/home/tw/tmp/nsx/internal-192/501171.nxs",
+        "/home/tw/tmp/nsx/internal-192/501172.nxs", "/home/tw/tmp/nsx/internal-192/501173.nxs",
+        "/home/tw/tmp/nsx/internal-192/501174.nxs", "/home/tw/tmp/nsx/internal-192/501175.nxs",
+        "/home/tw/tmp/nsx/internal-192/501176.nxs", "/home/tw/tmp/nsx/internal-192/501177.nxs",
+        "/home/tw/tmp/nsx/internal-192/501178.nxs", "/home/tw/tmp/nsx/internal-192/501179.nxs",
+        "/home/tw/tmp/nsx/internal-192/501180.nxs", "/home/tw/tmp/nsx/internal-192/501181.nxs",
+        "/home/tw/tmp/nsx/internal-192/501182.nxs", "/home/tw/tmp/nsx/internal-192/501183.nxs",
+        "/home/tw/tmp/nsx/internal-192/501184.nxs", "/home/tw/tmp/nsx/internal-192/501185.nxs",
+    };
+
     nsx::Diffractometer* diffractometer = nsx::Diffractometer::create("D19");
-    std::shared_ptr<nsx::DataSet> datafile;
-    try {
-        datafile =
-            factory.create("nxs", "/home/tw/tmp/nsx/internal-192/501168.nxs", diffractometer);
-    } catch (const std::exception& ex) {
-        std::cerr << "Skipping test because data file is not available." << std::endl;
-        return;
+    std::vector<std::shared_ptr<nsx::DataSet>> datafiles;
+
+    for (const std::string& file : files) {
+        std::cout << "Loading " << file << "..." << std::endl;
+
+        std::ifstream ifstr(file);
+        if (!ifstr.is_open()) {
+            std::cerr << "Skipping test because the data files are not available" << std::endl;
+            return;
+        }
+
+        std::shared_ptr<nsx::DataSet> datafile = factory.create("nxs", file, diffractometer);
+        datafile->open();
+        datafiles.push_back(datafile);
     }
 
-    datafile->open();
-    Eigen::MatrixXi v = datafile->frame(0);
+    Eigen::MatrixXi v = datafiles[0]->frame(0);
     CHECK((v.rows() == 256 && v.cols() == 640));
     // std::cout << v << std::endl;
 
     // CHECK(v.sum() == xxxxxxx);
-    datafile->close();
+
+    for (std::shared_ptr<nsx::DataSet> datafile : datafiles)
+        datafile->close();
 }
