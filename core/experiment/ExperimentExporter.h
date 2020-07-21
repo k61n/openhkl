202319da0af11d584736267c7ec995d0605f7459
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2020-07-21 15:38:18 +0200

    exporter: unpointerize hdf5 objects

--- core/experiment/ExperimentExporter.h
+++ core/experiment/ExperimentExporter.h
@@ -3,7 +3,7 @@
 //  NSXTool: data reduction for neutron single-crystal diffraction
 //
 //! @file      core/experiment/ExperimentExporter.h
-//! @brief     Class to export NSXTool to file
+//! @brief     Defines class ExperimentExporter.
 //!
 //! @homepage  ###HOMEPAGE###
 //! @license   GNU General Public License v3 or higher (see COPYING)
