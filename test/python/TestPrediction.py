707e8fd918fe7111750b29cef154f167564a70b5
Raza, Zamaan <z.raza@fz-juelich.de>
2020-08-05 17:18:13 +0200

    Added peak prediction test
    - WARNING: does not test anything yet!

---
+++ test/python/TestPrediction.py
@@ -0,0 +1,58 @@
+import sys
+import glob
+import unittest
+import pynsx as nsx
+
+class TestAutoIndexer(unittest.TestCase):
+
+    def test(self):
+        # Initialise the experiment
+        name = "FutA"
+        detector = "BioDiff5000"
+        expt = nsx.Experiment(name, detector)
+
+        expt.loadFromFile("FutA-indexed.nsx")
+        found_peaks = expt.getPeakCollection("found")
+        filtered_peaks = expt.getPeakCollection("found")
+        expt.assignUnitCell(found_peaks)
+        expt.assignUnitCell(filtered_peaks)
+
+        # build the shape library
+        data = expt.getAllData()
+        shapelib_params = nsx.ShapeLibParameters()
+        shapelib_params.kabsch = True
+        shapelib_params.sigma_m = 0.23
+        shapelib_params.sigma_d = 0.33
+        shapelib_params.nx = 20
+        shapelib_params.ny = 20
+        shapelib_params.nz = 20
+        shapelib_params.peak_scale = 3.0
+        shapelib_params.d_min = 1.5
+        shapelib_params.d_max = 50.0
+        shapelib_params.bkg_begin = 3.0
+        shapelib_params.bkg_end = 6.0
+        expt.buildShapeLibrary(filtered_peaks, shapelib_params)
+        nprofiles = filtered_peaks.shapeLibrary().numberOfPeaks()
+
+        # predict the peaks
+        prediction_params = nsx.PredictionParameters()
+        prediction_params.detector_range_min = 1.5
+        prediction_params.detector_range_max = 50.0
+        prediction_params.neighbour_max_radius = 400.0
+        prediction_params.frame_range_max = 20.0
+        prediction_params.bkg_begin = 3.0
+        prediction_params.bkg_end = 4.5
+        prediction_params.peak_scale = 5.0
+        prediction_params.set_fit_center = True
+        prediction_params.fit_covariance = True
+        prediction_params.min_neighbours = 400.0
+        expt.predictPeaks("predicted", filtered_peaks, prediction_params,
+                        nsx.PeakInterpolation_NoInterpolation)
+        predicted_peaks = expt.getPeakCollection("predicted")
+        expt.integratePredictedPeaks('1d profile integrator', predicted_peaks,
+                                    filtered_peaks.shapeLibrary(), prediction_params)
+
+
+if __name__ == "__main__":
+    # test()
+    unittest.main()
