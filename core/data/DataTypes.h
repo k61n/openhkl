2b746e30b3753b2c558b69cbe50c06fcf2ff1274
Raza, Zamaan <z.raza@fz-juelich.de>
2020-11-23 19:36:14 +0100

    Changed number of options in data loader menu

--- core/data/DataTypes.h
+++ core/data/DataTypes.h
@@ -24,6 +24,8 @@ class DataSet;
 using sptrDataSet = std::shared_ptr<DataSet>;
 using DataList = std::vector<sptrDataSet>;
 
+enum class DataFormat { HDF5, RAW, NEXUS, Count };
+
 } // namespace nsx
 
 #endif // NSX_CORE_DATA_DATATYPES_H
