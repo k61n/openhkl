0c91e1d66f4bbc9f4c31342405e128346dc379b1
Matthias Puchner <github@mpuchner.de>
2021-04-05 08:25:28 +0200

    rectify FindBlosc to meet the CMake standards; rectify usage of Blosc findings as well to meet these standards

--- cmake/FindBlosc.cmake
+++ cmake/FindBlosc.cmake
@@ -1,6 +1,47 @@
+
+#[=======================================================================[.rst:
+
+FindBlosc
+---------
+
+Finds the Blosc library.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``Blosc_FOUND``
+  True if the system has the Blosc library.
+``Blosc_VERSION``
+  The version of the Blosc library which was found.
+``Blosc_INCLUDE_DIRS``
+  Include directories needed to use Blosc.
+``Blosc_LIBRARIES``
+  Libraries needed to link to Blosc.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Blosc_INCLUDE_DIR``
+  The directory containing ``blosc.h``.
+``Blosc_LIBRARY``
+  The path to the Blosc library.
+
+#]=======================================================================]
+
+
 find_path(Blosc_INCLUDE_DIR blosc.h)
 
 find_library(Blosc_LIBRARY NAMES blosc)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Blosc DEFAULT_MSG Blosc_LIBRARY Blosc_INCLUDE_DIR)
+
+# Create the variables required by CMake conventions
+if(Blosc_FOUND)
+  set(Blosc_LIBRARIES ${Blosc_LIBRARY})
+  set(Blosc_INCLUDE_DIRS ${Blosc_INCLUDE_DIR})
+endif()
