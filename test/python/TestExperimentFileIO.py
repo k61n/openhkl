da128aeadbff8ff5d7b43ddfb6739aa2ab105c70
Raza, Zamaan <z.raza@fz-juelich.de>
2021-05-04 18:03:07 +0200

    Repaired tests to run with new HDF5 format
    - Added hash for new (to be uploaded) test file FutA.nsx

--- test/python/TestExperimentFileIO.py
+++ test/python/TestExperimentFileIO.py
@@ -12,7 +12,7 @@ class TestExperimentFileIO(unittest.TestCase):
 
         # Reference data
         expt_ref = nsx.Experiment(name, detector)
-        expt_ref.loadFromFile("FutA-indexed.nsx")
+        expt_ref.loadFromFile("FutA.nsx")
         expt_ref.saveToFile("test.nsx") # test ExperimentExporter
         found_peaks_ref = expt_ref.getPeakCollection("found")
         filtered_peaks_ref = expt_ref.getPeakCollection("filtered")
