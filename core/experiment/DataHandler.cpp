k61n/openhkl

commit 48ffc8e9b6a9d2c9ce17d345a772d54c930766b1

Disallow null pointers to diffractometer instruments
All low-level data-readers assume a _valid_ (raw) pointer to an
instance of diffractometer instrument (see eg., `core/raw/IDataReader`
or `core/loader/HDF5MetaDataReader`). Therefore, this logical
requirement must be imposed at a high level to prevent undefined
behaviour.

This validity condition is currently verified in the constructor of
`core/experiment/DataHandler` and an exception is thrown in case of an
invalid instrument.

The exception is caught in the GUI session, `gui/models/Session::loadData`.

Resolves issue [224](https://jugit.fz-juelich.de/mlz/nsxtool/-/issues/224)

core/experiment/DataHandler.cpp
@@ -26,6 +26,14 @@ DataHandler::DataHandler(const std::string& name, const std::string& diffractome
     if (!(diffractometerName == std::string("unknown_instrument")))
         _diffractometer.reset(Diffractometer::create(diffractometerName));
     _name = name;
+
+    // For all data-readers, a valid diffractometer instrument is needed;
+    // `_diffractometer` must not be null; otherwise undefined behaviour might occur
+    if (!_diffractometer) {
+        std::ostringstream msg;
+        msg << "DataHandler: Instrument '" << diffractometerName << "' is not valid";
+        throw std::logic_error(msg.str());
+    }
 }
 
 Diffractometer* DataHandler::getDiffractometer()

gui/models/Session.cpp
@@ -138,8 +138,16 @@ void Session::loadData(nsx::DataFormat format)
     loadDirectory = info.absolutePath();
     s.setValue("data", loadDirectory);
 
-    if (_currentProject < 0)
-        createExperiment();
+    if (_currentProject < 0) {
+	try {
+	    createExperiment();
+	} catch (const std::exception& ex) {
+            QString msg = QString("Loading file(s) failed with error: ")
+                + QString(ex.what()) + QString(".");
+            QMessageBox::critical(nullptr, "Error", msg);
+        }
+	return;
+    }
 
     for (const QString& filename : filenames) {
         QFileInfo fileinfo(filename);

core/raw/IDataReader.cpp
@@ -29,6 +29,8 @@ namespace nsx {
 IDataReader::IDataReader(const std::string& filename, Diffractometer* diffractometer)
     : _diffractometer(diffractometer)
     , _nFrames(0)
+    , _nRows(0)
+    , _nCols(0)
     , _sampleStates()
     , _detectorStates()
     , _fileSize(0)

commit 62ac3e1819cd7c5f168883e0ca86135775ca5ff2

Eliminate bugs in initializing an Experiment
The bugs caused by commit (48ffc8e9) prevented loading Experiments.

The check for the validity of the pointer to an instance of
diffractometer is moved to `Session::loadData`, so that `Experiment` can
be instantiated with an empty (null) diffractometer.

It is still possible to cause undefined behaviour by accessing a null
diffractometer pointer from `Experiment`, esp. in low-level data-readers.

The error message is slightly improved.

core/experiment/DataHandler.cpp
@@ -26,14 +26,6 @@ DataHandler::DataHandler(const std::string& name, const std::string& diffractome
     if (!(diffractometerName == std::string("unknown_instrument")))
         _diffractometer.reset(Diffractometer::create(diffractometerName));
     _name = name;
-
-    // For all data-readers, a valid diffractometer instrument is needed;
-    // `_diffractometer` must not be null; otherwise undefined behaviour might occur
-    if (!_diffractometer) {
-        std::ostringstream msg;
-        msg << "DataHandler: Instrument '" << diffractometerName << "' is not valid";
-        throw std::logic_error(msg.str());
-    }
 }
 
 Diffractometer* DataHandler::getDiffractometer()

gui/models/Session.cpp
@@ -140,16 +140,8 @@ void Session::loadData(nsx::DataFormat format)
     loadDirectory = info.absolutePath();
     s.setValue("data", loadDirectory);
 
-    if (_currentProject < 0) {
-	try {
-	    createExperiment();
-	} catch (const std::exception& ex) {
-            QString msg = QString("Loading file(s) failed with error: ")
-                + QString(ex.what()) + QString(".");
-            QMessageBox::critical(nullptr, "Error", msg);
-        }
-	return;
-    }
+    if (_currentProject < 0)
+        createExperiment();
 
     for (const QString& filename : filenames) {
         QFileInfo fileinfo(filename);
@@ -157,9 +149,15 @@ void Session::loadData(nsx::DataFormat format)
 
         // If the experiment already stores the current numor, skip it
         if (exp->hasData(filename.toStdString()))
-            return; // nullptr;
+            return;
 
         try {
+            // For all data-readers, a valid diffractometer instrument is needed;
+            // `_diffractometer` must not be null; otherwise undefined behaviour might occur
+            if (!exp->getDiffractometer()) {
+                throw std::runtime_error("Please set a valid instrument first");
+            }
+
             nsx::sptrDataSet data_ptr;
 
             std::string extension = fileinfo.completeSuffix().toStdString();
@@ -167,10 +165,11 @@ void Session::loadData(nsx::DataFormat format)
                 extension, filename.toStdString(), exp->getDiffractometer());
             exp->addData(data_ptr);
         } catch (const std::exception& ex) {
-            QString msg = QString("Loading file \"") + filename + QString("\" failed with error: ")
+            QString msg = QString("Loading file(s) '") + filename + QString("' failed with error: ")
                 + QString(ex.what()) + QString(".");
 
             QMessageBox::critical(nullptr, "Error", msg);
+	    return;
         }
     }
     currentProject()->selectData(currentProject()->getIndex(filenames.at(0)));