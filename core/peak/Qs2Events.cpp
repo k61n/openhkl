579aa27c757d2220a3b71d20013a3df44d86aaaf
Raza, Zamaan <z.raza@fz-juelich.de>
2021-06-02 13:24:20 +0000

    Fix d min issue

--- core/peak/Qs2Events.cpp
+++ core/peak/Qs2Events.cpp
@@ -13,6 +13,8 @@
 //  ***********************************************************************************************
 
 #include "core/peak/Qs2Events.h"
+
+#include "base/utils/Logger.h"
 #include "core/data/DataSet.h" // TODO mv interpolatedState
 #include "core/instrument/InterpolatedState.h"
 
@@ -22,9 +24,32 @@ std::vector<DetectorEvent> algo::qs2events(
     const std::vector<ReciprocalVector>& sample_qs, const InstrumentStateList& states,
     const Detector& detector)
 {
+    nsxlog(Level::Info, "algo::Qs2Events::qs2events: processing", sample_qs.size(), "q vectors");
+
+    /*
+     * Algorithm description
+     *
+     * For each q-vector corresponding to a Miller index:
+     * 1. Divide the frame range into n_intervals equal intervals
+     * 2. for each interval f0 -> f1:
+     *    a) compute sign of |k_f| - |k_i| for f0 (s0) and f1 (s1). 
+     *    b) if s1 == s2, continue
+     *    c) else use bisection search to compute frame value f at which |k_f| = |k_i|
+     * 3. For each intersection f, generate a DetectorEvent, which will be used as a peak centre
+     */
+
+    // Tolerance for bisection search
+    const double eps = 1.0e-10;
+    // Divide frame range into this number of intervals, do bisection search on each interval
+    const int n_intervals = 4; // TODO: generalise this arbitrary parameter. May always work.
+
+    const double fmin = 0.0;
+    const double fmax = states.size() - 2;
+
+    std::vector<double> roots;
     std::vector<DetectorEvent> events;
 
-    // return true if inside Ewald sphere, false otherwise
+    // return true if |k_f| < |k_i|, i.e q_vect is inside Ewald sphere
     auto compute_sign = [](const Eigen::RowVector3d& q, const InterpolatedState& state) -> bool {
         const Eigen::RowVector3d ki = state.ki().rowVector();
         const Eigen::RowVector3d kf = ki + q * state.sampleOrientationMatrix().transpose();
@@ -35,48 +60,59 @@ std::vector<DetectorEvent> algo::qs2events(
     for (const ReciprocalVector& sample_q : sample_qs) {
         const Eigen::RowVector3d& q_vect = sample_q.rowVector();
 
-        double f0 = 0.0;
-        double f1 = states.size() - 2;
+        roots.clear();
 
-        InterpolatedState state0 = states.interpolate(f0);
-        InterpolatedState state1 = states.interpolate(f1);
+        for (int i = 0; i < n_intervals; ++i) { // Iterate over equally sized intervals
 
-        bool s0 = compute_sign(q_vect, state0);
-        const bool s1 = compute_sign(q_vect, state1);
+            // Generate the interval
+            double f0 = fmin + double(i) * fmax/double(n_intervals);
+            double f1 = f0 + fmax/double(n_intervals);
 
-        // does not cross Ewald sphere, or crosses more than once
-        if (s0 == s1)
-            continue;
+            InterpolatedState state0 = states.interpolate(f0);
+            InterpolatedState state1 = states.interpolate(f1);
 
-        // now use bisection method to compute intersection to good accuracy
-        while (f1 - f0 > 1e-10) {
-            double f = 0.5 * (f0 + f1);
-            InterpolatedState state = states.interpolate(f);
-            bool sign = compute_sign(q_vect, state);
+            bool s0 = compute_sign(q_vect, state0);
+            bool s1 = compute_sign(q_vect, state1);
 
-            if (sign == s0) { // branch right
-                s0 = sign;
-                state0 = state;
-                f0 = f;
-            } else { // branch left
-                state1 = state;
-                f1 = f;
+            // does not cross Ewald sphere, or crosses more than once
+            if (s0 == s1)
+                continue;
+
+            // now use bisection method to compute intersection to good accuracy
+            while (f1 - f0 > eps) {
+                double f = 0.5 * (f0 + f1);
+                InterpolatedState state = states.interpolate(f);
+                bool sign = compute_sign(q_vect, state);
+
+                if (sign == s0) { // branch right
+                    s0 = sign;
+                    state0 = state;
+                    f0 = f;
+                } else { // branch left
+                    s1 = sign;
+                    state1 = state;
+                    f1 = f;
+                }
             }
+            // store the frame value at the intersection
+            roots.push_back(0.5 * (f0 + f1));
         }
-        // now f stores the frame value at the intersection
-        const double f = 0.5 * (f0 + f1);
-        const auto state = states.interpolate(f);
 
-        Eigen::RowVector3d kf =
-            state.ki().rowVector() + q_vect * state.sampleOrientationMatrix().transpose();
-        DetectorEvent event = detector.constructEvent(
-            DirectVector(state.samplePosition), ReciprocalVector(kf * state.detectorOrientation),
-            f);
-        if (event._tof <= 0)
-            continue;
+        for (const double& frame : roots) { // Generate an event for each frame value
+            const auto state = states.interpolate(frame);
 
-        events.emplace_back(event);
+            Eigen::RowVector3d kf =
+                state.ki().rowVector() + q_vect * state.sampleOrientationMatrix().transpose();
+            DetectorEvent event = detector.constructEvent(
+                DirectVector(state.samplePosition), ReciprocalVector(kf * state.detectorOrientation),
+                frame);
+            if (event._tof <= 0)
+                continue;
+
+            events.emplace_back(event);
+        }
     }
+    nsxlog(Level::Info, "algo::Qs2Events::qs2events: finished; generated", events.size(), "events");
     return events;
 }
 
--- core/peak/Qs2Events.h
+++ core/peak/Qs2Events.h
@@ -22,6 +22,23 @@ namespace nsx {
 
 namespace algo {
 
+/*! \brief Determine the sample rotation(s) at which the Ewald sphere is intersected
+ *
+ * This algorithm determines the sample rotation angle (i.e. non-integer frame number) at which
+ * the Ewald sphere is intersected by a q-vector. Given a list of q-vectors corresponding to Miller
+ * indices, it will return a list of detector events (i.e. peak centres), determining the correct
+ * angle (frame) for each.
+ *
+ * For each q-vector, we determine whether the sign of |k_f| - |k_i| is different between the first
+ * and last frames. If it is, we use a bisection search to determine the angle (frame value) at
+ * at which |k_f| = |k_i|, and use this value to get an interpolated instrument state, and then
+ * A detector event (peak centre).
+ *
+ * Zamaan 2/6/2021 - in the original version, it was assumed that there is *one* root in the entire
+ * frame range, which is incorrect. I have fixed this by splitting the range into n_interval equal
+ * intervals and then performing the bisection algorithm over each interval.
+ */
+
 //! Returns detector events corresponding to the list of q values.
 std::vector<DetectorEvent> qs2events(
     const std::vector<ReciprocalVector>& sample_qs, const InstrumentStateList& states,
