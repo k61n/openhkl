053710d7256507211d9159a19c227120c0e74199
Ammar Nejati <a.nejati@fz-juelich.de>
2021-06-08 11:15:38 +0200

    Add a dialog to confirm overwriting a name/id

---
+++ gui/dialogs/ConfirmOverwriteDialog.cpp
@@ -0,0 +1,43 @@
+//  ***********************************************************************************************
+//
+//  NSXTool: data reduction for neutron single-crystal diffraction
+//
+//! @file      gui/dialogs/ConfirmOverwriteDialog.cpp
+//! @brief     Implements class ConfirmOverwriteDialog
+//!
+//! @homepage  ###HOMEPAGE###
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+//! @authors   see CITATION, MAINTAINER
+//
+//  ***********************************************************************************************
+
+#include "gui/dialogs/ConfirmOverwriteDialog.h"
+
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QString>
+
+ConfirmOverwriteDialog::ConfirmOverwriteDialog(const QString& name)
+{
+    setModal(true);
+    resize(500, 130);
+    setMinimumSize(500, 130);
+    setMaximumSize(500, 130);
+
+    const std::string msg {"The name '" + name.toStdString() + "' will be overwritten."};
+    QLabel* const dataname_label {new QLabel(QString::fromStdString(msg))};
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox();
+    buttonBox->setOrientation(Qt::Horizontal);  
+    buttonBox->setStandardButtons(QDialogButtonBox::No | QDialogButtonBox::Yes);
+
+    QVBoxLayout* vLayout = new QVBoxLayout(this);
+    vLayout->addWidget(dataname_label);
+    vLayout->addWidget(buttonBox);
+
+    QObject::connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    QObject::connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+    this->setLayout(vLayout);
+}
---
+++ gui/dialogs/ConfirmOverwriteDialog.h
@@ -0,0 +1,27 @@
+//  ***********************************************************************************************
+//
+//  NSXTool: data reduction for neutron single-crystal diffraction
+//
+//! @file      gui/dialogs/ConfirmOverwriteDialog.h
+//! @brief     Defines class ConfirmOverwriteDialog
+//!
+//! @homepage  ###HOMEPAGE###
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+//! @authors   see CITATION, MAINTAINER
+//
+//  ***********************************************************************************************
+
+#ifndef NSX_GUI_DIALOGS_CONFIRMOVERWRITEDIALOG_H
+#define NSX_GUI_DIALOGS_CONFIRMOVERWRITEDIALOG_H
+
+#include <QDialog>
+#include <QString>
+
+//! Dialog to get the experiment and instrument names
+class ConfirmOverwriteDialog : public QDialog {
+ public:
+    ConfirmOverwriteDialog(const QString& name);
+};
+
+#endif // NSX_GUI_DIALOGS_CONFIRMOVERWRITEDIALOG_H
