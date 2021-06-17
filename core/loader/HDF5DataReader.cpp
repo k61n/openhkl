342e48b9046a830941628da4ae49fd20a8c3a785
Ammar Nejati <a.nejati@fz-juelich.de>
2021-06-17 15:29:05 +0200

    HDF5DataReader.data: Minor improvements to the code

--- core/loader/HDF5DataReader.cpp
+++ core/loader/HDF5DataReader.cpp
@@ -26,16 +26,16 @@ HDF5DataReader::HDF5DataReader(const std::string& filename, Diffractometer* diff
 {
 }
 
-Eigen::MatrixXi HDF5DataReader::data(size_t frame)
+Eigen::MatrixXi HDF5DataReader::data(const std::size_t frame)
 {
     if (!_isOpened)
         open();
     // HDF5 specification requires row-major storage
-    Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> m(_nRows, _nCols);
-
-    hsize_t count[3] = {1, _nRows, _nCols};
-    hsize_t offset[3] = {frame, 0, 0};
-    _space->selectHyperslab(H5S_SELECT_SET, count, offset, nullptr, nullptr);
+    using RowMatrixXi = Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+    RowMatrixXi m(_nRows, _nCols);
+    const hsize_t count_1frm[3] = {1, _nRows, _nCols}; // a single frame
+    const hsize_t offset[3] = {frame, 0, 0};
+    _space->selectHyperslab(H5S_SELECT_SET, count_1frm, offset);
     _dataset->read(m.data(), H5::PredType::NATIVE_INT32, *_memspace, *_space);
     // return copy as MatrixXi (which could be col-major)
     return Eigen::MatrixXi(m);
