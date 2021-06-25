306198777d8b8c124f6b848f3b372820ff515e6a
Ammar Nejati <a.nejati@fz-juelich.de>
2021-06-25 14:23:33 +0200

    Add data keys as global constants defined in a _single_ header file

---
+++ core/raw/DataKeys.h
@@ -0,0 +1,102 @@
+//  ***********************************************************************************************
+//
+//  NSXTool: data reduction for neutron single-crystal diffraction
+//
+//! @file      core/raw/DataKeys.h
+//! @brief     Defines keys for storing the data
+//!
+//! @homepage  ###HOMEPAGE###
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+//! @authors   see CITATION, MAINTAINER
+//
+//  ***********************************************************************************************
+
+#ifndef NSX_CORE_RAW_DATAKEYS_H
+#define NSX_CORE_RAW_DATAKEYS_H
+
+#include <string>
+
+namespace nsx {
+
+// TODO: Add documentation: Describe the keywords and their units (if any)
+
+//-- HDF5 Group keys
+const std::string gr_DataCollections { "DataCollections" };
+const std::string gr_Detector { "Detector" };
+const std::string gr_Sample { "Sample" };
+const std::string gr_Metadata { "Metadata" };
+const std::string gr_PeakCollections { "PeakCollections" };
+const std::string gr_UnitCells { "UnitCells" };
+
+//-- HDF5 DataSet keys
+const std::string ds_DatasetNames { "DatasetNames" };
+const std::string ds_UnitCellNames { "UnitCellNames" };
+// peaks
+const std::string ds_PeakEnd { "PeakEnd" };
+const std::string ds_BkgBegin { "BkgBegin" };
+const std::string ds_BkgEnd { "BkgEnd" };
+const std::string ds_Scale { "Scale" };
+const std::string ds_Transmission { "Transmission" };
+const std::string ds_Intensity { "Intensity" };
+const std::string ds_Sigma { "Sigma" };
+const std::string ds_BkgIntensity { "BkgIntensity" };
+const std::string ds_BkgSigma { "BkgSigma" };
+const std::string ds_Center { "Center" };
+const std::string ds_Metric { "Metric" };
+const std::string ds_Rejection { "Rejection" };
+const std::string ds_Selected { "Selected" };
+const std::string ds_Masked { "Masked" };
+const std::string ds_Predicted { "Predicted" };
+
+//-- HDF Attribute keys
+// general
+const std::string at_experiment { "experiment" };
+const std::string at_diffractometer { "diffractometer" };
+const std::string at_datasetName { "dataset" };
+const std::string at_formatVersion { "NSX-format version" };
+const std::string at_filepath { "filepath" };
+const std::string at_datasetSources { "sources" };  // list of sources for the DataSet, separated by ';'
+const std::string at_framesNr { "number of frames" };
+const std::string at_wavelength { "wavelength" };
+const std::string at_monitorSum { "monitor sum" };  // TODO: explain
+// Nexus-specific metadata
+const std::string at_numor { "NUMOR" }; // internal raw data labelling of ILL
+const std::string at_totalSteps { "total steps" };
+const std::string at_title { "title" };
+const std::string at_time { "time" };
+const std::string at_startTime { "start time" };
+const std::string at_endTime { "end time" };
+// peaks
+const std::string at_peaksNr { "number of peaks" };
+const std::string at_validsNr { "number of valids" };
+const std::string at_invalidsNr { "number of invalids" };
+const std::string at_peakType { "peak type" };
+// unit cell
+const std::string at_rVec { "reciprocal" };
+const std::string at_BravaisLattice { "Bravais lattice" };
+const std::string at_indexingTol { "indexing tolerance" };
+const std::string at_spacegroup { "spacegroup" };
+const std::string at_z { "z" };
+
+//-- Default names
+const std::string kw_experimentName0 { "EXPERIMENT" };  // default name for an experiment
+const std::string kw_diffractometerName0 { "UNKNOWN INSTRUMENT" };  // default name for an experiment
+const std::string kw_datasetName0 { "DATASET" };  // default name for an experiment
+const std::string kw_unitcellName0 { "UNITCELL" };  // default name for a unit-cell
+const std::string kw_monochromatorName0 { "MONOCHROMATOR" };  // default name for a monochromator
+const std::string kw_unknownInstrument { "UNKNOWN INSTRUMENT" };  // unknown instrument/diffractometer
+
+//-- Axis names
+// detector gonio
+const std::string ax_2thetaGamma { "2theta(gamma)" };
+const std::string ax_2thetaNu { "2theta(nu)" };
+// sample gonio
+const std::string ax_omega { "omega" };
+const std::string ax_chi { "chi" };
+const std::string ax_phi { "phi" };
+
+
+} // namespace nsx
+
+#endif // NSX_CORE_RAW_DATAKEYS_H
