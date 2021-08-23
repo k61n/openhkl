5717e77b141bd75118a357bd80edcc491857aad3
Ammar Nejati <a.nejati@fz-juelich.de>
2021-08-23 10:47:49 +0200

    Implement ValueTupleItem to hold and sort table items which include a stdev. or quality measure
    `ValueTupleItem` is a `QStandardItem` which holds a floating-point value and its
    standard deviation or quality measure.
    The sorting mechanism is taylored to sort according the value and then (if the
    values are equal) the std. devs. or quality measures.
    Sorting is based on the stored _numerical_ values, _not_ the textual representation.
    
    * Sorting examples:
    ValueTupleItem("1.2(3)", 1.238, 3e-3) < ValueTupleItem("1.0(3)", 1.01, 3e-3)
    ValueTupleItem("1.2(3)", 1.238, 3e-3) < ValueTupleItem("1.2(3)", 1.240, 3e-3)
    ValueTupleItem("1.2(3)", 1.238, 3e-3) < ValueTupleItem("1.2(4)", 1.238, 4e-3)
    
    See the Qt documentation for `QStandardItem::operator<`.
    
    Resolves issue #90

--- gui/views/UnitCellTableView.h
+++ gui/views/UnitCellTableView.h
@@ -16,11 +16,37 @@
 #define NSX_GUI_VIEWS_UNITCELLTABLEVIEW_H
 
 #include <QTableView>
+#include <QString>
+#include <QStandardItem>
 
 class UnitCellTableView : public QTableView {
     Q_OBJECT
  public:
     explicit UnitCellTableView(QWidget* parent = 0);
 };
+
+//! Value-Tuple Item:
+//! A QStandardItem which holds a floating-point value and its standard deviation or quality measure.
+//! The sorting mechanism is taylored to sort according the value and then (if the values are equal) according the std. devs. or quality measures.
+//! Sorting is based on the stored numerical values, not the textual representation.
+//! * Sorting examples:
+//! ValueTupleItem("1.2(3)", 1.238, 3e-3) < ValueTupleItem("1.0(3)", 1.01, 3e-3)
+//! ValueTupleItem("1.2(3)", 1.238, 3e-3) < ValueTupleItem("1.2(3)", 1.240, 3e-3)
+//! ValueTupleItem("1.2(3)", 1.238, 3e-3) < ValueTupleItem("1.2(4)", 1.238, 4e-3)
+class ValueTupleItem: public QStandardItem
+{
+public:
+    explicit ValueTupleItem(const QString& text, const double val, const double qmes = 0);
+    ~ValueTupleItem() = default;
+
+    //! Custom sorting;
+    //! See the Qt documentation for `QStandardItem::operator<`.
+    bool operator< (const QStandardItem& other) const override;
+
+public:
+    double value = 0;
+    double qmeasure = 0;
+};
+
 
 #endif // NSX_GUI_VIEWS_UNITCELLTABLEVIEW_H
