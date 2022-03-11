8d698f308869a5df1f3798dcf206df7219939eba
Christian Trageser <c.trageser@fz-juelich.de>
2022-03-11 14:44:04 +0100

    switching projects at one single click

--- gui/subframe_home/SubframeHome.cpp
+++ gui/subframe_home/SubframeHome.cpp
@@ -151,12 +151,9 @@ void SubframeHome::_setRightLayout(QHBoxLayout* main_layout)
     _open_experiments_model = std::make_unique<ExperimentModel>();
     _open_experiments_view = new ExperimentTableView();
     _open_experiments_view->setModel(_open_experiments_model.get());
-    /*connect(
-        _open_experiments_view, &ExperimentTableView::doubleClicked, this,
-        &SubframeHome::_switchCurrentExperiment);*/
     connect(
         _open_experiments_view, &ExperimentTableView::clicked, this,
-        &SubframeHome::toggleUnsafeWidgets);
+        &SubframeHome::_switchCurrentExperiment);
 
     right->addWidget(_open_experiments_view);
 
