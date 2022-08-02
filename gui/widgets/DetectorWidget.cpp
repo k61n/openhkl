efbf24eb40bdbb693e31128c1a6b43fe389df946
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2022-08-02 17:21:10 +0200

    DetectorWidget, _spin and _scroll: Correct conversion between displayed and internal index

--- gui/widgets/DetectorWidget.cpp
+++ gui/widgets/DetectorWidget.cpp
@@ -169,15 +169,15 @@ void DetectorWidget::refresh()
 
     auto data = _data_combo->currentData();
 
-    scene()->slotChangeSelectedData(data, _spin->value());
+    scene()->slotChangeSelectedData(data, _spin->value() - 1);
     scene()->clearPeakItems();
     scene()->drawPeakitems();
     scene()->update();
     _detector_view->fitInView(scene()->sceneRect(), Qt::KeepAspectRatio);
-    _scroll->setMinimum(0);
+    _scroll->setMinimum(1);
     _scroll->setMaximum(data->nFrames());
 
-    _spin->setMinimum(0);
+    _spin->setMinimum(1);
     _spin->setMaximum(data->nFrames());
 
     emit scene()->dataChanged();
