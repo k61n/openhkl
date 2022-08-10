213684aa7f3bfcfe28dbf6630b8c4cdec3b01dac
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2022-08-10 15:26:57 +0200

    insert shebang on top of all Python tests

--- test/python/nightly/TestTrypsin.py
+++ test/python/nightly/TestTrypsin.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ##  ***********************************************************************************************
 ##
 ##  OpenHKL: data reduction for single crystal diffraction
