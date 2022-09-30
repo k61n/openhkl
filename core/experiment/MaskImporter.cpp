a2e77d8c2a60cdd81abdaccb5216c14727b7816f
Christian Trageser <c.trageser@fz-juelich.de>
2022-09-30 13:15:02 +0200

    added class to import detector masks from yml file

---
+++ core/experiment/MaskImporter.cpp
@@ -0,0 +1,64 @@
+//  ***********************************************************************************************
+//
+//  OpenHKL: data reduction for single crystal diffraction
+//
+//! @file      core/experiment/MaskImporter
+//! @brief     Defines class MaskImporter
+//!
+//! @homepage  ###HOMEPAGE###
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+//! @authors   see CITATION, MAINTAINER
+//
+//  ***********************************************************************************************
+
+#include "MaskImporter.h"
+#include <fstream>
+
+namespace ohkl{
+MaskImporter::MaskImporter(std::string filename)
+{
+    _node = YAML::LoadFile(filename.c_str());
+    int size = _node["size"].as<int>();
+
+    for (int i = 0; i < size; ++i){
+        std::string name = generateName(i);
+        std::string type = _node[name]["Type"].as<std::string>();
+
+        IMask* mask;
+        Eigen::Vector3d upper;
+        Eigen::Vector3d lower;
+ 
+        upper[0] = _node[name]["UpperX"].as<float>();
+        upper[1] = _node[name]["UpperY"].as<float>();
+        upper[2] = 150;
+
+        lower[0] =  _node[name]["LowerX"].as<float>();
+        lower[1] =  _node[name]["LowerY"].as<float>();
+        lower[2] = 0;
+
+        AABB aabb(upper, lower);
+
+        if (type == "Rectangle")
+            mask = new BoxMask(aabb);
+        else if (type == "Ellipse")
+            mask = new EllipseMask(aabb);
+        else 
+            throw std::runtime_error("E MaskImporter::MaskImporter Invalid Mask type found");
+
+        _masks.emplace_back(mask);
+    } 
+}
+
+std::string MaskImporter::generateName(int number)
+{
+    int n = 2; // number of digits
+    std::string str = std::to_string(number + 1);
+    return std::string("Mask") + std::string(n - str.size(), '0').append(str);
+}
+
+std::vector<IMask*> MaskImporter::getMasks()
+{
+    return _masks;
+} 
+}//ohkl
---
+++ core/experiment/MaskImporter.h
@@ -0,0 +1,42 @@
+//  ***********************************************************************************************
+//
+//  OpenHKL: data reduction for single crystal diffraction
+//
+//! @file      core/experiment/MaskImporter
+//! @brief     Defines class MaskImporter
+//!
+//! @homepage  ###HOMEPAGE###
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+//! @authors   see CITATION, MAINTAINER
+//
+//  ***********************************************************************************************
+
+#ifndef OHKL_CORE_MASK_IMPORTER_H
+#define OHKL_CORE_MASK_IMPORTER_H
+
+#include "base/mask/IMask.h"
+#include "base/mask/BoxMask.h"
+#include "base/mask/EllipseMask.h"
+#include "base/utils/YAMLType.h"
+
+#include <set>
+#include <string>
+
+namespace ohkl {
+class MaskImporter{
+public:
+    //! constructs the yaml nodes from given mask data
+    MaskImporter(std::string filename);
+    //! generate Mask name from index
+    std::string generateName(int number);
+    //! returns restored masks
+    std::vector<IMask*> getMasks();
+private:
+    YAML::Node _node;
+    std::vector<IMask*> _masks;
+};
+
+} // ohkl
+
+#endif
