e912d4b254bae9f10ec252c75138b58bbd071cc7
Raza, Zamaan <z.raza@fz-juelich.de>
2023-01-11 08:58:54 +0100

    Reduce minimum sidebar icon size

--- gui/utility/SideBar.h
+++ gui/utility/SideBar.h
@@ -66,7 +66,7 @@ class SideBar : public QWidget {
     QAction* mCheckedAction;
     QAction* mOverAction;
 
-    const unsigned int _min_icon_height = 80;
+    const unsigned int _min_icon_height = 60;
 };
 
 #endif // OHKL_GUI_UTILITY_SIDEBAR_H
