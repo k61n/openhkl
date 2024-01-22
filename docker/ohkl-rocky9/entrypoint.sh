91c9298b9ea7fc618a5e80016e3fc3fc9181ac41
Raza, Zamaan <z.raza@fz-juelich.de>
2024-01-22 15:01:21 +0100

    Add entrypoing to rocky9

---
+++ docker/ohkl-rocky9/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -ne 4 ]; then
+    echo "Invalid number of arguments"
+    exit 1
+fi
+
+USERNAME=$1
+USERID=$2
+GROUPNAME=$3
+GROUPID=$4
+
+# Create the correct user and group to match the host's, so that
+# the X connection works properly.
+groupadd -g $GROUPID $GROUPNAME
+useradd -m -g $GROUPID -u $USERID $USERNAME
+
+su $USERNAME
