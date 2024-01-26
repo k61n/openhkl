f825759ac8dca14b78dbdcd365d3dadd1a4268c4
Zamaan Raza <z.raza@fz-juelich.de>
2024-01-26 08:48:42 +0100

    add gcc dependencies for gfortran

--- devtools/mac_install.sh
+++ devtools/mac_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+gcc_lib_dir="/opt/homebrew/Cellar/gcc/13.2.0/lib/gcc/current"
+
 macdeployqt main/OpenHKL.app
+cp ${gcc_lib_dir}/libgcc_s.1.1.dylib main/OpenHKL.app/Contents/Frameworks
+cp ${gcc_lib_dir}/libquadmath.0.dylib main/OpenHKL.app/Contents/Frameworks
 codesign --deep --force --verify --verbose --sign "Zamaan Raza" main/OpenHKL.app
 
 hdiutil create -size 300m -fs HFS+ -volname "OpenHKL" -attach ./OpenHKL.dmg
-mv OpenHKL.app /Volumes/OpenHKL/
+mv main/OpenHKL.app /Volumes/OpenHKL/
 rm -rf /Volumes/OpenHKL/.fseventsd
 ln -s /Applications/ /Volumes/OpenHKL/Applications
 hdiutil detach /Volumes/OpenHKL
