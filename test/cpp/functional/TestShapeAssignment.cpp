5341e8f4bc2fc3741a606149f5babae7739de39d
Zamaan Raza <z.raza@fz-juelich.de>
2025-01-03 15:49:52 +0100

    Fix TestShapeAssignment

--- test/cpp/functional/TestShapeAssignment.cpp
+++ test/cpp/functional/TestShapeAssignment.cpp
@@ -42,7 +42,7 @@
 TEST_CASE("test/data/TestShapeAssignment.cpp", "")
 {
     // reference values
-    const int ref_n_peaks = 59209;
+    const int ref_n_peaks = 59086;
     const int ref_n_integrated_peaks = 56186;
     const double ref_rpim_overall = 0.0774;
     const double ref_ccstar_overall = 0.9975;
