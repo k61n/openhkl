7425edcc39b7bfcea9550b4a44de910e4cbb0ab3
Zamaan Raza <z.raza@fz-juelich.de>
2025-05-07 15:13:07 +0200

    Add rescaler test

---
+++ test/cpp/unit/rescale/TestRescaler.cpp
@@ -0,0 +1,48 @@
+//  ***********************************************************************************************
+//
+//  OpenHKL: data reduction for single crystal diffraction
+//
+//! @file      test/cpp/data/TestRescaler.cpp
+//! @brief     Test ...
+//!
+//! @homepage  https://openhkl.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Institut Laue-Langevin and Forschungszentrum Jülich GmbH 2016-
+//! @authors   see CITATION, MAINTAINER
+//
+//  ***********************************************************************************************
+
+#include "core/experiment/Experiment.h"
+#include "core/shape/PeakCollection.h"
+#include "tables/crystal/SpaceGroup.h"
+#include "tables/crystal/UnitCell.h"
+
+#include "test/cpp/catch.hpp"
+
+#include "core/rescale/Rescaler.h"
+
+#include <iostream>
+
+
+TEST_CASE("test/data/TestRescaler.cpp", "")
+{
+    const std::string filename = "Trypsin-small.ohkl";
+    ohkl::Experiment experiment("Trypsin", "BioDiff");
+    experiment.loadFromFile(filename);
+
+    ohkl::sptrDataSet data = experiment.getData("Scan I");
+    ohkl::PeakCollection* peaks = experiment.getPeakCollection("predicted");
+    ohkl::UnitCell* cell = experiment.getUnitCell("indexed");
+
+    std::vector<ohkl::PeakCollection*> collections = {peaks};
+
+    ohkl::Rescaler rescaler(collections, cell->spaceGroup(), true, true);
+    std::optional<double> minf = rescaler.rescale();
+    CHECK(minf);
+
+    if (minf)
+        std::cout << "minf = " << minf.value() << std::endl;
+
+    CHECK(false);
+
+}
